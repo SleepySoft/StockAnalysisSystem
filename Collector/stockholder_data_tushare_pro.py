7b91190e7a4fb5d5e0b57c0eaf0a7d7deb1f7022
SleepySoft <sleepysoft@gmail.com>
2020-02-25 23:07:05 +0800

    Update stockholder statistics

--- Collector/stockholder_data_tushare_pro.py
+++ Collector/stockholder_data_tushare_pro.py
@@ -154,7 +154,7 @@ def __fetch_stock_holder_data(**kwargs) -> pd.DataFrame:
     return result
 
 
-def __fetch_stock_holder_statistics(**kwargs) -> pd.DataFrame or None:
+def __fetch_stock_holder_statistics_full(**kwargs) -> pd.DataFrame or None:
     uri = kwargs.get('uri')
     result = check_execute_test_flag(**kwargs)
 
@@ -163,18 +163,28 @@ def __fetch_stock_holder_statistics(**kwargs) -> pd.DataFrame or None:
         ts_code = pickup_ts_code(kwargs)
         since, until = normalize_time_serial(period, default_since(), today())
 
+        clock = Clock()
+        pro = ts.pro_api(config.TS_TOKEN)
+        time_iter = DateTimeIterator(since, until)
+
         ts_since = since.strftime('%Y%m%d')
         ts_until = until.strftime('%Y%m%d')
-
-        pro = ts.pro_api(config.TS_TOKEN)
-
-        clock = Clock()
-        delayer.delay()
-
-        # TODO: Top10 api can only fetch 100 items per one time (100 / 10 / 4 = 2.5Years)
         result_count = pro.stk_holdernumber(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
-        result_top10 = pro.top10_holders(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
-        result_top10_nt = pro.top10_floatholders(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
+
+        result_top10 = None
+        result_top10_nt = None
+        while not time_iter.end():
+            # Top10 api can only fetch 100 items per one time (100 / 10 / 4 = 2.5Years)
+            sub_since, sub_until = time_iter.iter_years(2.4)
+            ts_since = sub_since.strftime('%Y%m%d')
+            ts_until = sub_until.strftime('%Y%m%d')
+            delayer.delay()
+
+            result_top10_part = pro.top10_holders(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
+            result_top10_nt_part = pro.top10_floatholders(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
+
+            result_top10 = pd.concat([result_top10, result_top10_part], sort=False)
+            result_top10_nt = pd.concat([result_top10_nt, result_top10_nt_part], sort=False)
 
         print('%s: [%s] - Network finished, time spending: %sms' % (uri, ts_code, clock.elapsed_ms()))
 
@@ -191,29 +201,81 @@ def __fetch_stock_holder_statistics(**kwargs) -> pd.DataFrame or None:
         result_top10_nt_grouped = pd.DataFrame({'stockholder_top10_nt': result_top10_nt.groupby(key_columns).apply(
             lambda x: x.drop(key_columns, axis=1).to_dict('records'))}).reset_index()
 
-        result = merge_on_columns(result_count, result_top10_grouped, key_columns)
-        result = merge_on_columns(result, result_top10_nt_grouped, key_columns)
+        result = pd.merge(result_top10_grouped, result_top10_nt_grouped, how='outer', on=key_columns, sort=False)
+        result = pd.merge(result, result_count, how='outer', on=key_columns, sort=False)
 
         print(result)
 
     check_execute_dump_flag(result, **kwargs)
 
     if result is not None:
-        result.fillna(0.0)
-        if uri == 'Stockholder.PledgeStatus':
-            result['due_date'] = result['end_date']
-            result['total_share'] = result['total_share'] * 10000
-            result['rest_pledge'] = result['rest_pledge'] * 10000
-            result['unrest_pledge'] = result['unrest_pledge'] * 10000
-            result['pledge_count'] = result['pledge_count'].astype(np.int64)
-            result['pledge_ratio'] = result['pledge_ratio'].astype(float)
-        elif uri == 'Stockholder.PledgeHistory':
-            result['due_date'] = result['ann_date']
-            result['pledge_amount'] = result['pledge_amount'] * 10000
-            result['holding_amount'] = result['holding_amount'] * 10000
-            result['pledged_amount'] = result['pledged_amount'] * 10000
+        result.fillna('')
+        result['period'] = pd.to_datetime(result['end_date'])
+        result['stock_identity'] = result['ts_code']
+        result['stock_identity'] = result['stock_identity'].str.replace('.SH', '.SSE')
+        result['stock_identity'] = result['stock_identity'].str.replace('.SZ', '.SZSE')
 
-        result['due_date'] = pd.to_datetime(result['due_date'])
+    return result
+
+
+# This method can fetch the whole data from 1990 to now, but it takes too much of time (50s for 000001)
+def __fetch_stock_holder_statistics_full(**kwargs) -> pd.DataFrame or None:
+    uri = kwargs.get('uri')
+    result = check_execute_test_flag(**kwargs)
+
+    if result is None:
+        period = kwargs.get('period')
+        ts_code = pickup_ts_code(kwargs)
+        since, until = normalize_time_serial(period, default_since(), today())
+
+        clock = Clock()
+        pro = ts.pro_api(config.TS_TOKEN)
+        time_iter = DateTimeIterator(since, until)
+
+        ts_since = since.strftime('%Y%m%d')
+        ts_until = until.strftime('%Y%m%d')
+        result_count = pro.stk_holdernumber(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
+
+        result_top10 = None
+        result_top10_nt = None
+        while not time_iter.end():
+            # Top10 api can only fetch 100 items per one time (100 / 10 / 4 = 2.5Years)
+            sub_since, sub_until = time_iter.iter_years(2.4)
+            ts_since = sub_since.strftime('%Y%m%d')
+            ts_until = sub_until.strftime('%Y%m%d')
+            delayer.delay()
+
+            result_top10_part = pro.top10_holders(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
+            result_top10_nt_part = pro.top10_floatholders(ts_code=ts_code, start_date=ts_since, end_date=ts_until)
+
+            result_top10 = pd.concat([result_top10, result_top10_part], sort=False)
+            result_top10_nt = pd.concat([result_top10_nt, result_top10_nt_part], sort=False)
+
+        print('%s: [%s] - Network finished, time spending: %sms' % (uri, ts_code, clock.elapsed_ms()))
+
+        if result_count is None or result_top10 is None or result_top10_nt is None:
+            print('Fetch stockholder statistics data fail.')
+            return None
+
+        del result_top10['ann_date']
+        del result_top10_nt['ann_date']
+
+        key_columns = ['ts_code', 'end_date']
+        result_top10_grouped = pd.DataFrame({'stockholder_top10': result_top10.groupby(key_columns).apply(
+            lambda x: x.drop(key_columns, axis=1).to_dict('records'))}).reset_index()
+        result_top10_nt_grouped = pd.DataFrame({'stockholder_top10_nt': result_top10_nt.groupby(key_columns).apply(
+            lambda x: x.drop(key_columns, axis=1).to_dict('records'))}).reset_index()
+
+        result = pd.merge(result_top10_grouped, result_top10_nt_grouped, how='outer', on=key_columns, sort=False)
+        result = pd.merge(result, result_count, how='outer', on=key_columns, sort=False)
+
+        print(result)
+
+    check_execute_dump_flag(result, **kwargs)
+
+    if result is not None:
+        result.fillna('')
+        result['period'] = pd.to_datetime(result['end_date'])
         result['stock_identity'] = result['ts_code']
         result['stock_identity'] = result['stock_identity'].str.replace('.SH', '.SSE')
         result['stock_identity'] = result['stock_identity'].str.replace('.SZ', '.SZSE')
