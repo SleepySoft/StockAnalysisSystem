36a2fc8813edaa5fae3b17cfd38f1210606c9098
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-03-29 11:27:57 +0800

    Add analyser to cash
    Change-Id: I4c7fd5bb696d9116a1c6c97ec5fe8e800a9b6be3

--- Analyzer/analyzer_tang.py
+++ Analyzer/analyzer_tang.py
@@ -9,6 +9,7 @@ try:
     from Utiltity.common import *
     from Utiltity.df_utility import *
     from Utiltity.time_utility import *
+    from Utiltity.digit_utility import *
     from Analyzer.AnalyzerUtility import *
     from DataHub.DataHubEntry import DataHubEntry
     from Database.DatabaseEntry import DatabaseEntry
@@ -19,6 +20,7 @@ except Exception as e:
     from Utiltity.common import *
     from Utiltity.df_utility import *
     from Utiltity.time_utility import *
+    from Utiltity.digit_utility import *
     from Analyzer.AnalyzerUtility import *
     from DataHub.DataHubEntry import DataHubEntry
     from Database.DatabaseEntry import DatabaseEntry
@@ -53,10 +55,6 @@ portrait_comments_table = {
 }
 
 
-def __can_ignore_or_negative(value: float, reference: float) -> bool:
-    return value < 0.0 or (abs(value) / reference < 0.1)
-
-
 def analyzer_stock_portrait(securities: str, data_hub: DataHubEntry,
                             database: DatabaseEntry, context: AnalysisContext) -> AnalysisResult:
     nop(database)
@@ -86,7 +84,6 @@ def analyzer_stock_portrait(securities: str, data_hub: DataHubEntry,
 
     df = pd.merge(df_balance, df_cash, how='left', on=['stock_identity', 'period'])
 
-    score = 100
     reason = []
     portraits = {}
     for index, row in df.iterrows():
@@ -98,14 +95,14 @@ def analyzer_stock_portrait(securities: str, data_hub: DataHubEntry,
             continue
 
         portrait = \
-            ('-' if __can_ignore_or_negative(row['经营活动产生的现金流量净额'], net_assets) else '+') + \
-            ('-' if __can_ignore_or_negative(row['投资活动产生的现金流量净额'], net_assets) else '+') + \
-            ('-' if __can_ignore_or_negative(row['筹资活动产生的现金流量净额'], net_assets) else '+')
+            ('-' if can_ignore_or_negative(row['经营活动产生的现金流量净额'], net_assets, 0.1) else '+') + \
+            ('-' if can_ignore_or_negative(row['投资活动产生的现金流量净额'], net_assets, 0.1) else '+') + \
+            ('-' if can_ignore_or_negative(row['筹资活动产生的现金流量净额'], net_assets, 0.1) else '+')
         if portrait in portraits.keys():
             portraits[portrait] += 1
         else:
             portraits[portrait] = 1
-        reason.append('%s : %s' % (period, portrait))
+        reason.append('%s : %s' % (period.year, portrait))
 
     portrait_counts = len(portraits)
     if portrait_counts > 0:
@@ -124,11 +121,98 @@ def analyzer_stock_portrait(securities: str, data_hub: DataHubEntry,
         return AnalysisResult(securities, AnalysisResult.SCORE_NOT_APPLIED, '无足够数据')
 
 
+def analyzer_check_monetary_fund(securities: str, data_hub: DataHubEntry,
+                                 database: DatabaseEntry, context: AnalysisContext) -> AnalysisResult:
+    nop(database)
+
+    if context.cache.get('securities_info', None) is None:
+        context.cache['securities_info'] = data_hub.get_data_center().query('Market.SecuritiesInfo')
+    df_info = context.cache.get('securities_info', None)
+
+    df_slice = df_info[df_info['stock_identity'] == securities]
+    industry = get_dataframe_slice_item(df_slice, 'industry', 0, '')
+    if industry in ['银行', '保险', '房地产', '全国地产', '区域地产']:
+        return AnalysisResult(securities, AnalysisResult.SCORE_NOT_APPLIED, '不适用于此行业')
+
+    fields_balance_sheet = ['货币资金', '资产总计', '负债合计',
+                            '短期借款', '一年内到期的非流动负债', '其他流动负债',
+                            '长期借款', '应付债券', '其他非流动负债',
+                            '应收票据', '其他流动负债', '流动负债合计',
+                            '交易性金融资产', '可供出售金融资产']
+    df_balance_sheet, result = query_readable_annual_report_pattern(
+        data_hub, 'Finance.BalanceSheet', securities, (years_ago(5), now()), fields_balance_sheet)
+    if result is not None:
+        return result
+
+    score = []
+    reason = []
+    for index, row in df_balance_sheet.iterrows():
+        period = row['period']
+
+        var = dict(row)
+
+        # Why '其他流动负债' is a DataFrame in df_balance_sheet
+        if '其他流动负债' in var.keys():
+            value = var.get('其他流动负债')
+            var['其他流动负债'] = value[0]
+
+        var['净资产'] = var['资产总计'] - var['负债合计']
+        var['短期负债'] = var['短期借款'] + var['一年内到期的非流动负债'] + var['其他流动负债']
+        var['有息负债'] = var['短期负债'] + var['长期借款'] + var['应付债券'] + var['其他非流动负债']
+        var['金融资产'] = var['交易性金融资产'] + var['可供出售金融资产']
+
+        if var['有息负债'] > 1:
+            var['货币资金/有息负债'] = var['货币资金'] / var['有息负债']
+            reason.append('%s : 货币资金/有息负债 = %s' % (period.year, format_pct(var['货币资金/有息负债'])))
+
+            if var['货币资金/有息负债'] < 2.0:
+                score.append(0)
+            elif var['货币资金/有息负债'] < 3.0:
+                score.append(60)
+            else:
+                score.append(100)
+        else:
+            score.append(100)
+            reason.append('%s : 无有息负债' % period.year)
+
+        if var['短期负债'] > 1:
+            var['货币资金/短期负债'] = var['货币资金'] / var['短期负债']
+            if var['货币资金/短期负债'] < 1.0:
+                reason.append('%s : 货币资金/短期负债 = %s, 小于1' % (period.year, format_pct(var['货币资金/短期负债'])))
+                score.append(0)
+            else:
+                score.append(100)
+        else:
+            score.append(100)
+            reason.append('%s : 无短期负债' % period.year)
+
+        var['有息负债/资产总计'] = var['有息负债'] / var['资产总计']
+        var['货币资金+金融资产'] = var['货币资金'] + var['金融资产']
+
+        if var['有息负债/资产总计'] > 0.6:
+            score.append(0)
+            reason.append('%s 有息负债/资产总计 = %s, 大于 60%%' % (period.year, format_pct(var['有息负债/资产总计'])))
+        else:
+            score.append(100)
+
+        if var['货币资金+金融资产'] < var['有息负债']:
+            score.append(0)
+            reason.append('%s : 货币资金+金融资产 = %s 小于 有息负债 %s' %
+                          (period.year, format_w(var['货币资金+金融资产']), format_w(var['有息负债'])))
+        else:
+            score.append(100)
+
+    if len(score) > 0:
+        return AnalysisResult(securities, int(float(sum(score)) / float(len(score))), reason)
+    else:
+        return AnalysisResult(securities, AnalysisResult.SCORE_NOT_APPLIED, '无数据')
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 METHOD_LIST = [
-    ('3ee3a4ff-a2cf-4244-8f45-c319016ee16b', '[Tang] 现金流肖像', '根据经营现金流,投资现金流,筹资现金流的情况为企业绘制画像',       analyzer_stock_portrait),
-    ('7e132f82-a28e-4aa9-aaa6-81fa3692b10c', '', '',       None),
+    ('3ee3a4ff-a2cf-4244-8f45-c319016ee16b', '[T001] 现金流肖像',    '根据经营现金流,投资现金流,筹资现金流的情况为企业绘制画像',       analyzer_stock_portrait),
+    ('7e132f82-a28e-4aa9-aaa6-81fa3692b10c', '[T002] 货币资金分析',  '分析货币资金，详见excel中的对应的ID行',                         analyzer_check_monetary_fund),
     ('7b0478d3-1e15-4bce-800c-6f89ee743600', '', '',       None),
     ('fff6c3cf-a6e5-4fa2-9dce-7d0566b581a1', '', '',       None),
     ('d2ced262-7a03-4428-9220-3d4a2a8fe201', '', '',       None),
