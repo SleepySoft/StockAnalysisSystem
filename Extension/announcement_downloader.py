e382c38f22c6c117f9ec8a43513df08ece56745a
SleepySoft <sleepysoft@gmail.com>
2020-04-21 22:33:23 +0800

    Add ui for report downloader.

--- Extension/announcement_downloader.py
+++ Extension/announcement_downloader.py
@@ -1,31 +1,26 @@
-import logging
-import requests
-import random
 import time
 import urllib
-import matplotlib
-from pylab import mpl
-from os import sys, path
-from PyQt5.QtCore import pyqtSignal, QProcess
-from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QLineEdit, QFileDialog, QComboBox, QVBoxLayout
+import random
+import logging
+import requests
+import datetime
+from os import sys, path, makedirs
 
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt5.QtCore import Qt, QTimer, QDateTime
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QComboBox, QDateTimeEdit, QCheckBox
 
 root_path = path.dirname(path.dirname(path.abspath(__file__)))
 
 try:
-    from Analyzer.AnalyzerUtility import *
-    from DataHub.DataHubEntry import DataHubEntry
-    from Database.DatabaseEntry import DatabaseEntry
-    from stock_analysis_system import StockAnalysisSystem
+    from Utiltity.common import *
+    from Utiltity.ui_utility import *
+    from Utiltity.time_utility import *
 except Exception as e:
     sys.path.append(root_path)
 
-    from Analyzer.AnalyzerUtility import *
-    from DataHub.DataHubEntry import DataHubEntry
-    from Database.DatabaseEntry import DatabaseEntry
-    from stock_analysis_system import StockAnalysisSystem
+    from Utiltity.common import *
+    from Utiltity.ui_utility import *
+    from Utiltity.time_utility import *
 finally:
     pass
 
@@ -124,74 +119,128 @@ class AnnouncementDownloader:
         namelist = requests.post(query_path, headers=headers, data=query)
         return namelist.json()['announcements']  # json中的年度报告信息
 
-    def execute_download(self, queries, include: [str], exclude: [str]):
-        if queries is None:
+    @staticmethod
+    def execute_download(page_data, include_filter: [str] or None = None, exclude_filter: [str] or None = None):
+        if page_data is None:
             return
 
-        headers = {'Accept': 'application/json, text/javascript, */*; q=0.01',
-                   "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                   "Accept-Encoding": "gzip, deflate",
-                   "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-HK;q=0.6,zh-TW;q=0.5",
-                   'Host': 'www.cninfo.com.cn',
-                   'Origin': 'http://www.cninfo.com.cn'
-                   }
+        # download_headers = {
+        #     'Accept': 'application/json, text/javascript, */*; q=0.01',
+        #     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        #     'Accept-Encoding': 'gzip, deflate',
+        #     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-HK;q=0.6,zh-TW;q=0.5',
+        #     'Host': 'www.cninfo.com.cn',
+        #     'Origin': 'http://www.cninfo.com.cn'
+        # }
+        # download_headers['User-Agent'] = random.choice(User_Agent)
+        download_path = 'http://static.cninfo.com.cn/'
 
-        for i in queries:
-            allowed_list = [
-                '2018年年度报告（更新后）',
-                '2018年年度报告',
-                '2017年年度报告（更新后）',
-                '2017年年度报告',
-                '2016年年度报告（更新后）',
-                '2016年年度报告',
-            ]
-            allowed_list_2 = [
-                '招股书',
-                '招股说明书',
-                '招股意向书',
-            ]
-            title = i['announcementTitle']
-            allowed = title in allowed_list
-            if '确认意见' in title:
-                return
-            for item in allowed_list_2:
-                if item in title:
-                    allowed = True
-                    break
-            if allowed:
-                download = download_path + i["adjunctUrl"]
-                name = i["secCode"] + '_' + i['secName'] + '_' + i['announcementTitle'] + '.pdf'
-                if '*' in name:
-                    name = name.replace('*', '')
-                file_path = saving_path + name
-                time.sleep(random.random() * 2)
+        for page in page_data:
+            title = page['announcementTitle']
 
-                headers['User-Agent'] = random.choice(User_Agent)
-                r = requests.get(download)
-
-                f = open(file_path, "wb")
-                f.write(r.content)
-                f.close()
+            allowed = False
+            if include_filter is not None and len(include_filter) > 0:
+                for inc in include_filter:
+                    if inc in title:
+                        allowed = True
+                        break
             else:
+                allowed = True
+
+            if exclude_filter is not None and len(exclude_filter) > 0:
+                for exc in exclude_filter:
+                    if exc in title:
+                        allowed = False
+                        break
+            print('Announcement <<' + title + '>> ' + ('Allowed' if allowed else 'Ignore'))
+            if not allowed:
                 continue
 
-    def format_download_path(self, queries) -> str:
-        pass
+            download = download_path + page["adjunctUrl"]
+            file_name = AnnouncementDownloader.format_download_path(page)
+
+            if '*' in file_name:
+                file_name = file_name.replace('*', '')
+
+            time.sleep(random.random() * 2)
+            r = requests.get(download)
+
+            f = open(file_name, "wb")
+            f.write(r.content)
+            f.close()
+
+    @staticmethod
+    def format_download_path(page) -> str:
+        file_name = page['secName'] + '_' + page['announcementTitle'] + '.pdf'
+        file_path = path.join(root_path, page['secCode'])
+        makedirs(file_path, exist_ok=True)
+        return path.join(file_path, file_name)
+
+    # ----------------------------------------- Interface -----------------------------------------
+
+    @staticmethod
+    def download_annual_report(stock_identity: str or list, time_range: any = None):
+        if not isinstance(stock_identity, (list, tuple)):
+            stock_identity = [stock_identity]
+        for s in stock_identity:
+            if s.endwith('.SSE'):
+                s = s[: -4]
+                page_data = AnnouncementDownloader.build_query_for_sse_annual_report(s, time_range)
+            elif s.endwith('.SZSE'):
+                s = s[: -5]
+                page_data = AnnouncementDownloader.build_query_for_szse_annual_report(s, time_range)
+            else:
+                exchange = get_stock_exchange(s)
+                if exchange == 'SSE':
+                    page_data = AnnouncementDownloader.build_query_for_sse_annual_report(s, time_range)
+                elif exchange == 'SZSE':
+                    page_data = AnnouncementDownloader.build_query_for_szse_annual_report(s, time_range)
+                else:
+                    page_data = AnnouncementDownloader.build_query_for_sse_annual_report(s, time_range)
+            AnnouncementDownloader.execute_download(page_data, 
+                                                    include_filter=['年年度报告'],
+                                                    exclude_filter=['确认意见'])
+
+
+# allowed_list = [
+#     '2018年年度报告（更新后）',
+#     '2018年年度报告',
+#     '2017年年度报告（更新后）',
+#     '2017年年度报告',
+#     '2016年年度报告（更新后）',
+#     '2016年年度报告',
+# ]
+# allowed_list_2 = [
+#     '招股书',
+#     '招股说明书',
+#     '招股意向书',
+# ]
+#
+# exclude = [
+#     '确认意见'
+# ]
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 class AnnouncementDownloaderUi(QWidget):
-    def __init__(self, datahub_entry: DataHubEntry):
+    def __init__(self):
         super(AnnouncementDownloaderUi, self).__init__()
 
-        # ---------------- ext var ----------------
+        # Timer for update stock list
+        self.__timer = QTimer()
+        self.__timer.setInterval(1000)
+        self.__timer.timeout.connect(self.on_timer)
+        self.__timer.start()
 
-        self.__data_hub = datahub_entry
-        self.__data_center = self.__data_hub.get_data_center() if self.__data_hub is not None else None
-        self.__data_utility = self.__data_hub.get_data_utility() if self.__data_hub is not None else None
+        # Ui component
+        self.__combo_name = QComboBox()
+        self.__button_download = QPushButton('确定')
+        self.__check_annual_report = QCheckBox('年报')
+        self.__check_customize = QCheckBox('自定义')
 
-        # ------------- plot resource -------------
+        self.__datetime_since = QDateTimeEdit(QDateTime.currentDateTime())
+        self.__datetime_until = QDateTimeEdit(QDateTime.currentDateTime())
 
         self.init_ui()
 
@@ -205,11 +254,25 @@ class AnnouncementDownloaderUi(QWidget):
         main_layout = QVBoxLayout()
         self.setLayout(main_layout)
 
+        main_layout.addLayout(horizon_layout([QLabel('股票代码'), self.__combo_name]))
+        main_layout.addLayout(horizon_layout([QLabel('报告起始'), self.__datetime_since]))
+        main_layout.addLayout(horizon_layout([QLabel('报告截至'), self.__datetime_until]))
+        main_layout.addLayout(horizon_layout([QLabel('报告类型'), self.__datetime_until]))
+
     def __config_control(self):
         self.__button_draw.clicked.connect(self.on_button_draw)
 
-    def on_button_draw(self):
-        self.plot()
+    def on_timer(self):
+        # Check stock list ready and update combobox
+        data_utility = self.__sas.get_data_hub_entry().get_data_utility()
+        if data_utility.stock_cache_ready():
+            stock_list = data_utility.get_stock_list()
+            for stock_identity, stock_name in stock_list:
+                self.__combo_name.addItem(stock_identity + ' | ' + stock_name, stock_identity)
+            self.__timer.stop()
+
+    def on_button_download(self):
+        pass
 
 
 # ----------------------------------------------------------------------------------------------------------------------
