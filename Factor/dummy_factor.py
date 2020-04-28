ca866e2c810e531a907e71224e4848a8f3214202
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-04-28 17:00:07 +0800

    Add factor center to data center.
    Change-Id: I4f0e04d151c40d2acf045672d45424c2ca043d99

--- Factor/dummy_factor.py
+++ Factor/dummy_factor.py
@@ -8,10 +8,10 @@ FACTOR_LIST = {
 
 def plugin_prob() -> dict:
     return {
-        'plugin_id': '',
+        'plugin_id': 'cc43c66a-dabd-45a8-9698-b32e1817536d',
         'plugin_name': 'DummyFactor',
         'plugin_version': '0.0.0.1',
-        'tags': ['Dummy', 'Sleepy'],
+        'tags': ['Dummy', 'Factor', 'Sleepy'],
         'factor': FACTOR_LIST,
     }
 
