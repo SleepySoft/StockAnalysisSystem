cf4578b5cbdd1ca2ed50bef90c76c3d4f6c50224
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-05-05 14:48:52 +0800

    Add factor calculation engine.
    Change-Id: Iae2c74977177c76140d581173710ddfd74daecc7

--- Factor/FactorUtility.py
+++ Factor/FactorUtility.py
@@ -1,3 +1,4 @@
+import re
 import openpyxl
 import datetime
 import pandas as pd
@@ -7,6 +8,7 @@ root_path = path.dirname(path.dirname(path.abspath(__file__)))
 try:
     import config
     from Utiltity.common import *
+    from Utiltity.dependency import *
     from Utiltity.df_utility import *
     from Utiltity.time_utility import *
     from DataHub.DataHubEntry import DataHubEntry
@@ -16,6 +18,7 @@ except Exception as e:
 
     import config
     from Utiltity.common import *
+    from Utiltity.dependency import *
     from Utiltity.df_utility import *
     from Utiltity.time_utility import *
     from DataHub.DataHubEntry import DataHubEntry
@@ -23,6 +26,137 @@ except Exception as e:
 finally:
     pass
 
+
+class FactorEngine:
+    """
+    The format of the formula:
+        [A] = ([B] + [C]) / [D] ; [D] = [E] + [F] ; [F] = [G] * [H]
+        The fields should be surrounded by []
+        Multiple expression should be separated by ;
+        The calculation should be supported by DataFrame
+    """
+
+    FIELD_FINDER = re.compile(r'(?<=\[).*?(?=\])')
+
+    def __init__(self, formula: str, comments: str):
+        self.__formula = formula
+        self.__comments = comments
+
+        # Need calculate
+        self.__depends = []
+        self.__provides = []
+        self.__eval_formula = ''
+
+        self.__parse_formula()
+
+    def prob(self) -> ():
+        return self.provides(), self.depends(), self.comments(), self.entry(), None, None, None
+
+    def entry(self):
+        return self.calculate
+
+    def depends(self) -> list or str:
+        return self.__depends
+
+    def provides(self) -> list or str:
+        return self.__provides
+
+    def comments(self) -> str:
+        return self.__comments
+
+    def calculate(self, identity: str or [str], time_serial: tuple, mapping: dict,
+                  data_hub: DataHubEntry, database: DatabaseEntry, extra: dict) -> pd.DataFrame or None:
+        # Need implementation
+        assert False
+
+    def eval_formula(self) -> str:
+        return self.__eval_formula
+
+    def __parse_formula(self):
+        self.__formula.replace('\r', '')
+        self.__formula.replace('\n', ';')
+        expressions = self.__formula.split(';')
+
+        all_fields = []
+        result_expressions = {}
+        dependency = Dependency()
+
+        # Split and analysis the sub expressions
+        for exp in expressions:
+            if exp.strip() == '':
+                continue
+            provides, depends = self.__parse_single_expression(exp)
+            if len(provides) == 0 or len(depends) == 0:
+                continue
+            all_fields.extend(depends)
+            all_fields.append(provides)
+            result_expressions[provides] = exp
+            dependency.add_dependence(provides, depends)
+
+        # Sort the calculation chain
+        eval_exp_order = []
+        ordered_result = dependency.sort_by_dependency()
+        result_expressions_copy = result_expressions.copy()
+        for result in ordered_result:
+            if result in result_expressions_copy.keys():
+                eval_exp_order.append(result_expressions_copy.get(result))
+                del result_expressions_copy[result]
+        for key, val in result_expressions_copy.items():
+            eval_exp_order.append(val)
+        self.__eval_formula = '\n'.join(eval_exp_order)
+
+        self.__eval_formula = self.__eval_formula.replace('[', "df['")
+        self.__eval_formula = self.__eval_formula.replace(']', "']")
+
+        # Not only the final result, but also the mid result
+        self.__provides = list(result_expressions.keys())
+
+        # Calculate the outer depends
+        all_fields = list(set(all_fields))
+        self.__depends = [field for field in all_fields if field not in self.__provides]
+
+    @staticmethod
+    def __parse_single_expression(exp: str) -> (str, [str]):
+        parts = exp.split('=')
+        if len(parts) != 2:
+            print('Error: An Expression should has one "="')
+            return '', []
+
+        left = parts[0].strip()
+        right = parts[1].strip()
+        if len(left) == 0 or len(right) == 0:
+            print('Error: An Expression should not be empty in both side of "="')
+            return '', []
+
+        left_fields = FactorEngine.FIELD_FINDER.findall(left)
+        right_fields = FactorEngine.FIELD_FINDER.findall(right)
+        if len(left_fields) != 1 or len(right_fields) == 0:
+            print('Error: Left side of "=" should have only one field '
+                  'and right side of "=" should have at least one field')
+            return '', []
+
+        return left_fields[0], right_fields
+
+
+class FinanceFactorEngine(FactorEngine):
+    def __init__(self, formula: str, comments: str):
+        super(FinanceFactorEngine, self).__init__(formula, comments)
+
+    def calculate(self, identity: str or [str], time_serial: tuple, mapping: dict,
+                  data_hub: DataHubEntry, database: DatabaseEntry, extra: dict):
+        df = query_finance_pattern(data_hub, identity, time_serial, self.depends(), mapping)
+        eval_formula = self.eval_formula()
+
+        df['无息流动负债'] = df['应付账款'] + df['预收款项'] + df['应付职工薪酬'] + df['应交税费'] + df['其他应付款'] + df['预提费用'] + df['递延收益'] + df['其他流动负债']
+        df['净营运资本'] = df['应收账款'] + df['其他应收款'] + df['预付款项'] + df['存货'] - df['无息流动负债'] + df['长期股权投资'] + df['投资性房地产']
+        df['资本收益率'] = df['息税前利润'] / (df['净营运资本'] + df['固定资产'])
+        df['息税前利润'] = df['息税前利润'] + df['应付利息']
+
+        eval(eval_formula)
+        return df[self.provides()]
+
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 def dispatch_calculation_pattern(factor: str, identity: str or [str], time_serial: tuple, mapping: dict,
                                  data_hub: DataHubEntry, database: DatabaseEntry, extra: dict,
@@ -55,12 +189,43 @@ def query_finance_pattern(data_hub: DataHubEntry, identity: str,
     return df
 
 
+# ----------------------------------------------------- Test Entry -----------------------------------------------------
+
+def test_factor_engine():
+    fe = FactorEngine('[资本收益率] = [息税前利润] / ([净营运资本] + [固定资产]);'
+                      '[息税前利润] = [税前利润] + [利息费用];'
+                      '[净营运资本] = [应收账款] + [其他应收款] + [预付账款] + [存货] - [无息流动负债] + [长期股权投资] + [投资性房地产];'
+                      '[无息流动负债] = [应付账款] + [预收款项] + [应付职工薪酬] + [应交税费] + [其他应付款] + [预提费用] + [递延收益] + [其他流动负债];',
+                      '测试')
 
 
+# ----------------------------------------------------- File Entry -----------------------------------------------------
+
+def main():
+    test_factor_engine()
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+
+def exception_hook(type, value, tback):
+    # log the exception here
+    print('Exception hook triggered.')
+    print(type)
+    print(value)
+    print(tback)
+    # then call the default handler
+    sys.__excepthook__(type, value, tback)
 
 
-
+if __name__ == "__main__":
+    sys.excepthook = exception_hook
+    try:
+        main()
+    except Exception as e:
+        print('Error =>', e)
+        print('Error =>', traceback.format_exc())
+        exit()
+    finally:
+        pass
 
 
---
+++ Factor/amazing_formula_factor.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from datetime import date
+
+from os import sys, path
+root_path = path.dirname(path.dirname(path.abspath(__file__)))
+
+try:
+    from Factor.FactorUtility import *
+    from DataHub.DataHubEntry import DataHubEntry
+    from Database.DatabaseEntry import DatabaseEntry
+except Exception as e:
+    sys.path.append(root_path)
+
+    from Factor.FactorUtility import *
+    from DataHub.DataHubEntry import DataHubEntry
+    from Database.DatabaseEntry import DatabaseEntry
+finally:
+    pass
+
+
+engine_amazing_formula = FinanceFactorEngine(
+    '[资本收益率] = [息税前利润] / ([净营运资本] + [固定资产]);'
+    '[息税前利润] = [息税前利润] + [应付利息];'
+    '[净营运资本] = [应收账款] + [其他应收款] + [预付款项] + [存货] - [无息流动负债] + [长期股权投资] + [投资性房地产];'
+    '[无息流动负债] = [应付账款] + [预收款项] + [应付职工薪酬] + [应交税费] + [其他应付款] + [预提费用] + [递延收益] + [其他流动负债];',
+    '来自神奇公式网，用以评估公司的收益率')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+FACTOR_TABLE = {
+    '7ac29c74-3eaf-4c07-92a2-cc95785ff609': (engine_amazing_formula.prob()),
+}
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def plugin_prob() -> dict:
+    return {
+        'plugin_id': '',
+        'plugin_name': 'Amazing Formula Factor',
+        'plugin_version': '0.0.0.1',
+        'tags': ['Amazing', 'Sleepy'],
+        'factor': FACTOR_TABLE,
+    }
+
+
+def plugin_capacities() -> list:
+    return list(FACTOR_TABLE.keys())
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def calculate(factor: str, identity: str or [str], time_serial: tuple, mapping: dict,
+              data_hub: DataHubEntry, database: DatabaseEntry, extra: dict) -> pd.DataFrame or None:
+    return dispatch_calculation_pattern(factor, identity, time_serial, mapping, data_hub, database, extra, FACTOR_TABLE)
+
