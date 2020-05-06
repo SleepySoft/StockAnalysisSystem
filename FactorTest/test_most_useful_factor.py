5d8cad1596d6ddd4ec082256324122e9490d818c
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-05-06 16:27:33 +0800

    Add plot for factors.
    Change-Id: I6112d40f156df74d2f9cd2795d14bd655f3a8ae6

--- FactorTest/test_most_useful_factor.py
+++ FactorTest/test_most_useful_factor.py
@@ -13,7 +13,7 @@ finally:
     pass
 
 
-def __default_test_factor(sas: StockAnalysisSystem, factors: [str]):
+def __test_factor_for_one_stock(sas: StockAnalysisSystem, factors: [str]):
     if not isinstance(factors, (list, tuple)):
         factors = [factors]
     data_hub = sas.get_data_hub_entry()
@@ -27,28 +27,49 @@ def __default_test_factor(sas: StockAnalysisSystem, factors: [str]):
         assert fct in df.columns
 
 
+def __test_factor_for_all_stock(sas: StockAnalysisSystem, factors: [str]):
+    if not isinstance(factors, (list, tuple)):
+        factors = [factors]
+    data_hub = sas.get_data_hub_entry()
+    data_center = data_hub.get_data_center()
+    df = data_center.query_from_factor('Factor.Finance', '',
+                                       (text_auto_time('2018-12-01'), text_auto_time('2018-12-31')),
+                                       fields=factors, readable=True)
+    print(df)
+    assert df is not None and len(df) > 0
+
+    for fct in factors:
+        assert fct in df.columns
+
+
 def test_amazing_formula_factor(sas: StockAnalysisSystem):
-    __default_test_factor(sas, '资本收益率')
+    __test_factor_for_one_stock(sas, '资本收益率')
+    __test_factor_for_all_stock(sas, '资本收益率')
 
 
 def test_roe(sas: StockAnalysisSystem):
-    __default_test_factor(sas, '净资产收益率')
+    __test_factor_for_one_stock(sas, '净资产收益率')
+    __test_factor_for_all_stock(sas, '净资产收益率')
 
 
 def test_roa(sas: StockAnalysisSystem):
-    __default_test_factor(sas, '总资产收益率')
+    __test_factor_for_one_stock(sas, '总资产收益率')
+    __test_factor_for_all_stock(sas, '总资产收益率')
 
 
 def test_gross_margin(sas: StockAnalysisSystem):
-    __default_test_factor(sas, '毛利率')
+    __test_factor_for_one_stock(sas, '毛利率')
+    __test_factor_for_all_stock(sas, '毛利率')
 
 
 def test_operating_margin(sas: StockAnalysisSystem):
-    __default_test_factor(sas, '营业利润率')
+    __test_factor_for_one_stock(sas, '营业利润率')
+    __test_factor_for_all_stock(sas, '营业利润率')
 
 
 def test_net_profit_rate(sas: StockAnalysisSystem):
-    __default_test_factor(sas, '净利润率')
+    __test_factor_for_one_stock(sas, '净利润率')
+    __test_factor_for_all_stock(sas, '净利润率')
 
 
 def test_entry(sas: StockAnalysisSystem):
