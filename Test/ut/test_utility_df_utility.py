be5ac0c9b68de9266d89b2ab42af75e8bc6c76cf
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-05-20 13:23:36 +0800

    Add test case for utility.
    Change-Id: Ie4e3595c42ed477621384871a4309dcbe4a9fd51

---
+++ Test/ut/test_utility_df_utility.py
@@ -0,0 +1,59 @@
+import traceback
+import pandas as pd
+from os import sys, path
+root_path = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(root_path)
+
+from src.core.Utiltity.df_utility import check_date_continuity
+from src.core.Utiltity.df_utility import test_entry as test_entry_df_utility
+
+
+def test_check_date_continuity():
+    df = pd.read_csv(root_path + '/TestData/Market_TradeCalender.csv')
+    continuity, min_date, max_date = check_date_continuity(df, 'cal_date')
+    print('continuity = ' + str(continuity))
+    print('min_date = ' + str(min_date))
+    print('max_date = ' + str(max_date))
+
+
+def test_entry():
+    test_entry_df_utility()
+    test_check_date_continuity()
+
+
+def main():
+    test_entry()
+    print('All Test Passed.')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def exception_hook(type, value, tback):
+    # log the exception here
+    print('Exception hook triggered.')
+    print(type)
+    print(value)
+    print(tback)
+    # then call the default handler
+    sys.__excepthook__(type, value, tback)
+
+
+if __name__ == "__main__":
+    sys.excepthook = exception_hook
+    try:
+        main()
+    except Exception as e:
+        print('Error =>', e)
+        print('Error =>', traceback.format_exc())
+        exit()
+    finally:
+        pass
+
+
+
+
+
+
+
+
+
