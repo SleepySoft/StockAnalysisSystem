0f934ff5f0907d34ae98a97d0ffb906d0451bb66
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-05-22 10:22:44 +0800

    Add project path.
    Change-Id: I1b6a3009a67363866d3c55b087cde59a2b547133

--- StockAnalysisSystem/api.py
+++ StockAnalysisSystem/api.py
@@ -1,13 +1,32 @@
-from .core.StockAnalysisSystem import StockAnalysisSystem
+from StockAnalysisSystem.ui.main_ui import MainWindow
+from StockAnalysisSystem.ui.config_ui import ConfigUi
 from .core.Utiltity.plugin_manager import PluginManager
+from StockAnalysisSystem.core.Utiltity.ui_utility import *
+from StockAnalysisSystem.core.StockAnalysisSystem import StockAnalysisSystem
 
 
-def system_entry() -> StockAnalysisSystem:
+def run_ui():
+    app = QApplication(sys.argv)
+
+    sas = StockAnalysisSystem()
+    sas.check_initialize()
+
+    while not sas.is_initialized():
+        dlg = WrapperQDialog(ConfigUi(False))
+        dlg.exec()
+        sas.check_initialize()
+
+    main_wnd = MainWindow()
+    main_wnd.show()
+    sys.exit(app.exec())
+
+
+def sas() -> StockAnalysisSystem:
     return StockAnalysisSystem()
 
 
 def get_plugin_manager(name: str) -> PluginManager:
-    return system_entry().get_plugin_manager(name)
+    return sas().get_plugin_manager(name)
 
 
 
---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+import StockAnalysisSystem.api as sas_api
+
+
+def main():
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    sas_api.sas().check_initialize(project_path)
+    sas_api.run_ui()
+    print('Process Quit.')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def exception_hook(type, value, tback):
+    # log the exception here
+    print('Exception hook triggered.')
+    print(type)
+    print(value)
+    print(tback)
+    # then call the default handler
+    sys.__excepthook__(type, value, tback)
+
+
+if __name__ == "__main__":
+    sys.excepthook = exception_hook
+    try:
+        main()
+    except Exception as e:
+        print('Error =>', e)
+        print('Error =>', traceback.format_exc())
+        exit()
+    finally:
+        pass
