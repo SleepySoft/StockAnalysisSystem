2a32df49707063108b7f0a27a00d4e024655d0a4
SleepySoft <sleepysoft@gmail.com>
2020-06-23 22:28:06 +0800

    Update code

--- Test/manual/test_trader.py
+++ Test/manual/test_trader.py
@@ -1,4 +1,6 @@
-from StockAnalysisSystem.core.StockAnalysisSystem import StockAnalysisSystem
+import os
+
+import StockAnalysisSystem.api as sas_api
 from StockAnalysisSystem.core.Utiltity.time_utility import *
 
 from StockAnalysisSystem.core.Trader.Market import MarketBackTesting
@@ -6,8 +8,18 @@ from StockAnalysisSystem.core.Trader.TradeStrategy import Trader
 from StockAnalysisSystem.core.Trader.Broker import Broker
 
 
-sas = StockAnalysisSystem()
-market = MarketBackTesting(sas.get_data_hub_entry(), text_auto_time('2019-01-01'), text_auto_time('2020-01-01'))
+project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+sas_api.config_set('NOSQL_DB_HOST', 'localhost')
+sas_api.config_set('NOSQL_DB_PORT', '27017')
+sas_api.config_set('TS_TOKEN', 'xxxxxxxxxxxx')
+
+if not sas_api.init(project_path, True):
+    print('sas init fail.')
+    print('\n'.join(sas_api.error_log()))
+    quit(1)
+
+market = MarketBackTesting(sas_api.get_data_hub_entry(), text_auto_time('2019-01-01'), text_auto_time('2020-01-01'))
 broker = Broker()
 trader = Trader()
 
