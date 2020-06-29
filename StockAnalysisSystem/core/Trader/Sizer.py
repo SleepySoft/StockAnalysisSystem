dea1d72e9848d8c7b5b7aedc64541b3f034ed732
SleepySoft <sleepysoft@gmail.com>
2020-06-29 22:29:04 +0800

    Try add grid tradder.

--- StockAnalysisSystem/core/Trader/Sizer.py
+++ StockAnalysisSystem/core/Trader/Sizer.py
@@ -1,12 +1,63 @@
-from .Interface import IMarket, ISizer, Position
+from .Interface import IBroker, ISizer
 
 
-class FullPositionBuy(ISizer):
-    def __init__(self, security: str):
-        super(FullPositionBuy, self).__init__(security)
+class BuyCashPercent(ISizer):
+    def __init__(self, percent: float):
+        self.__percent = percent
+        super(BuyCashPercent, self).__init__()
 
-    def amount(self, market: IMarket, position: Position) -> int:
-        pass
+    def amount(self, security: str, price: float, broker: IBroker) -> int:
+        position = broker.position()
+        available_cash = position.cash()
+        buy_cash = available_cash * self.__percent
+        buy_amount = buy_cash / price
+        buy_amount = self.rounding(buy_amount / ISizer.MINIMAL_SHARE) * ISizer.MINIMAL_SHARE
+        return buy_amount
+
+
+class BuyPositionPercent(ISizer):
+    def __init__(self, percent: float):
+        self.__percent = percent
+        super(BuyPositionPercent, self).__init__()
+
+    def amount(self, security: str, price: float, broker: IBroker) -> int:
+        position = broker.position()
+        available_cash = position.cash()
+        security_cash = position.security_amount(security) * price
+        total_cash = available_cash + security_cash
+        buy_cash = total_cash * self.__percent
+        buy_cash = min(buy_cash, available_cash)
+        buy_amount = buy_cash / price
+        buy_amount = self.rounding(buy_amount / ISizer.MINIMAL_SHARE) * ISizer.MINIMAL_SHARE
+        return buy_amount
+
+
+class SellCashReference(ISizer):
+    def __init__(self, ref_cash: float):
+        self.__ref_cash = ref_cash
+        super(SellCashReference, self).__init__()
+
+    def amount(self, security: str, price: float, broker: IBroker) -> int:
+        position = broker.position()
+        ref_amount = self.__ref_cash / price
+        ref_amount = self.rounding(ref_amount / ISizer.MINIMAL_SHARE) * ISizer.MINIMAL_SHARE
+        sell_amount = min(ref_amount, position.security_amount(security))
+        return sell_amount
+
+
+class SellPositionPercent(ISizer):
+    def __init__(self, security: str, percent: float):
+        self.__percent = percent
+        super(SellPositionPercent, self).__init__(security)
+
+    def amount(self, security: str, price: float, broker: IBroker) -> int:
+        position = broker.position()
+        available_cash = position.cash()
+        security_cash = position.security_amount(security) * price
+        total_cash = available_cash + security_cash
+        sell_cash = total_cash * self.__percent
+        sizer = SellCashReference(sell_cash)
+        return sizer.amount(security, price, broker)
 
 
 
