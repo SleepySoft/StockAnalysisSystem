be40286fafcf89c4f3c584556e0ce58c4c452f61
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-06-30 17:00:54 +0800

    Stash.
    Change-Id: Ieda5315bbcebf9983987702f68b9632b07edc0a8

--- Test/manual/test_trader.py
+++ Test/manual/test_trader.py
@@ -4,7 +4,7 @@ import StockAnalysisSystem.api as sas_api
 from StockAnalysisSystem.core.Utiltity.time_utility import *
 
 from StockAnalysisSystem.core.Trader.Market import MarketBackTesting
-from StockAnalysisSystem.core.Trader.TradeStrategy import Trader
+from StockAnalysisSystem.core.Trader.TradeStrategy import *
 from StockAnalysisSystem.core.Trader.Broker import Broker
 
 
@@ -21,7 +21,7 @@ if not sas_api.init(project_path, True):
 
 market = MarketBackTesting(sas_api.get_data_hub_entry(), text_auto_time('2019-01-01'), text_auto_time('2020-01-01'))
 broker = Broker(market)
-trader = Trader(market, broker)
+trader = GridTrader(market, broker, '000001.SZSE', 7.0, 4.0, 5)
 
 market.load_back_testing_data('000001.SZSE', True)
 market.watch_security('000001.SZSE', broker)
