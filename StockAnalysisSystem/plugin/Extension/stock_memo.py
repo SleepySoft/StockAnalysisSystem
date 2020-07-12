acb83ec5c7968cfa45fb5e0b6e003bdb1bb1a04a
SleepySoft <sleepysoft@gmail.com>
2020-07-12 13:32:30 +0800

    Add tags lib.

--- StockAnalysisSystem/plugin/Extension/stock_memo.py
+++ StockAnalysisSystem/plugin/Extension/stock_memo.py
@@ -14,8 +14,10 @@ from PyQt5.QtWidgets import QButtonGroup, QDateTimeEdit, QApplication, QLabel, Q
     QMessageBox, QWidget, QComboBox, QCheckBox, QRadioButton, QLineEdit, QAbstractItemView
 from pyqtgraph.GraphicsScene.mouseEvents import MouseClickEvent
 
+from StockAnalysisSystem.core.config import *
 from StockAnalysisSystem.porting.vnpy_chart import *
 from StockAnalysisSystem.core.Utiltity.common import *
+from StockAnalysisSystem.core.Utiltity.CsvRecord import *
 from StockAnalysisSystem.core.Utiltity.df_utility import *
 from StockAnalysisSystem.core.Utiltity.ui_utility import *
 from StockAnalysisSystem.core.Utiltity.TableViewEx import *
@@ -48,126 +50,25 @@ def memo_path_from_project_path(project_path: str) -> str:
         return memo_path
 
 
-# ---------------------------------------------------- Class Record ----------------------------------------------------
+# ----------------------------------------------- class StockMemoRecord ------------------------------------------------
 
-class Record:
-    def __init__(self, record_path: str, record_columns: [str], must_columns: [str] or None = None):
-        self.__record_path = record_path
-        self.__record_columns = record_columns
-        self.__must_columns = must_columns
-        self.__record_sheet = pd.DataFrame(columns=self.__record_columns)
-
-    def columns(self) -> [str]:
-        return self.__record_columns
-
-    def is_empty(self) -> bool:
-        return self.__record_sheet is None or self.__record_sheet.empty
-
-    def add_record(self, _data: dict, _save: bool = True) -> (bool, int):
-        if not self.__check_must_columns(_data, True, True):
-            return False, -1
-        df = self.__record_sheet
-        max_index = max(df.index) if len(df) > 0 else 0
-        new_index = max_index + 1
-        row = [_data.get(k, '') for k in self.__record_columns]
-        df.loc[new_index, self.__record_columns] = tuple(row)
-        return (self.save() if _save else True), new_index
-
-    def update_record(self, index, _data: dict, _save: bool = True) -> bool:
-        if not self.__check_must_columns(_data, False, True):
-            return False
-        fields = list(_data.keys())
-        values = [_data.get(field, '') for field in fields]
-        df = self.__record_sheet
-        if index in df.index.values:
-            df.loc[index, fields] = tuple(values)
-        else:
-            print('Warning: Index %s not in record.' % str(index))
-            return False
-        return self.save() if _save else True
-
-    def get_records(self, conditions: dict or None = None) -> pd.DataFrame:
-        if conditions is None or len(conditions) == 0:
-            return self.__record_sheet
-        df = self.__record_sheet
-        # https://stackoverflow.com/a/40125748/12929244
-        mask = pd.concat([df[k].eq(v) for k, v in conditions.items()], axis=1).all(axis=1)
-        return df[mask]
-        # return df[np.logical_and.reduce([df[k] == v for k, v in conditions.items()])]
-
-    def del_records(self, idx: int or [int]):
-        self.__record_sheet.drop(idx)
-
-    def load(self, record_path: str = '') -> bool:
-        if record_path != '':
-            if self.__load(record_path):
-                self.__record_path = record_path
-                return True
-            else:
-                return False
-        return self.__load(self.__record_path)
-
-    def save(self, record_path: str = ''):
-        if record_path != '':
-            if self.__save(record_path):
-                self.__record_path = record_path
-                return True
-            else:
-                return False
-        return self.__save(self.__record_path)
-
-    # ---------------------------------------------------
-
-    def __load(self, record_path: str) -> bool:
-        try:
-            df = pd.read_csv(record_path)
-            df['time'] = pd.to_datetime(df['time'], infer_datetime_format=True)
-            df.reindex()
-            self.__record_sheet = df
-            return column_includes(self.__record_sheet.columns, self.__record_columns)
-        except Exception as e:
-            return False
-        finally:
-            pass
-
-    def __save(self, record_path: str) -> bool:
-        try:
-            self.__record_sheet = self.__record_sheet[self.__record_columns]
-            self.__record_sheet.to_csv(record_path)
-            return True
-        except Exception as e:
-            print('Save stock memo %s error.' % record_path)
-            print(e)
-            return False
-        finally:
-            pass
-
-    def __check_must_columns(self, _data: dict, check_exists: bool, check_empty: bool) -> bool:
-        if self.__must_columns is None or len(self.__must_columns) == 0:
-            return True
-        for must_column in self.__must_columns:
-            if must_column not in _data.keys():
-                if check_exists:
-                    return False
-            else:
-                _value = _data.get(must_column)
-                if check_empty and (_value is None or _value == ''):
-                    return False
-        return True
-
-    @staticmethod
-    def __check_record_format(_time: datetime.datetime, brief: str, content: str):
-        return isinstance(_time, datetime.datetime) and isinstance(brief, str) and isinstance(content, str)
-
-
-# ------------------------------------------------ class StockMemoData -------------------------------------------------
-
-class StockMemoRecord(Record):
+class StockMemoRecord(CsvRecord):
     ALL_COLUMNS = ['time', 'security', 'brief', 'content', 'classify']
     MUST_COLUMNS = ['time', 'security']
     
     def __init__(self, record_path: str):
         super(StockMemoRecord, self).__init__(record_path, StockMemoRecord.ALL_COLUMNS, StockMemoRecord.MUST_COLUMNS)
+
+    # --------------------- Override ---------------------
+
+    def load(self, record_path: str = '') -> bool:
+        ret = super(StockMemoRecord, self).load(record_path)
+        if ret:
+            df = self.get_records()
+            df['time'] = pd.to_datetime(df['time'], infer_datetime_format=True)
+        return ret
+
+    # ----------------------------------------------------
 
     def get_stock_memos(self, stock_identity: str):
         df = self.get_records({'security': stock_identity})
@@ -176,6 +77,10 @@ class StockMemoRecord(Record):
     def get_all_security(self) -> [str]:
         df = self.get_records()
         return df['security'].unique()
+
+
+# ----------------------------------------------------- class Tags -----------------------------------------------------
+
 
 
 # ------------------------------------------------ class StockMemoData -------------------------------------------------
@@ -202,11 +107,15 @@ class StockMemoData:
     def set_root_path(self, root_path):
         self.__root_path = root_path
 
+    # -------------- Core Object --------------
+
     def get_sas(self) -> StockAnalysisSystem:
         return self.__sas
 
-    def get_memo_record(self) -> Record:
+    def get_memo_record(self) -> StockMemoRecord:
         return self.__memo_record
+
+    # ------------- Extra Object --------------
 
     def set_data(self, name: str, _data: any):
         self.__extra_data[name] = _data
@@ -758,7 +667,7 @@ class StockHistoryUi(QWidget):
         enable = self.__check_memo.isChecked()
 
     def on_button_volume(self):
-        enable = self.on_button_volume.isChecked()
+        enable = self.__check_volume.isChecked()
 
     def on_button_ensure(self):
         input_securities = self.__combo_name.currentText()
