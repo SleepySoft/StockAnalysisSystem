86c1cf947d6f0b2d06a1f9c49aa342d899a43d98
SleepySoft <sleepysoft@gmail.com>
2020-07-12 16:13:51 +0800

    Add flow layout.

---
+++ StockAnalysisSystem/core/Utiltity/FlowLayout.py
@@ -0,0 +1,148 @@
+"""
+PyQt5 port of the `layouts/flowlayout
+<https://doc.qt.io/qt-5/qtwidgets-layouts-flowlayout-example.html>`_ example
+from Qt5.
+
+"""
+from PyQt5.QtCore import pyqtSignal, QPoint, QRect, QSize, Qt
+from PyQt5.QtWidgets import QLayout, QSizePolicy, QSpacerItem
+
+
+# https://stackoverflow.com/a/46727466
+
+class FlowLayout(QLayout):
+    """A ``QLayout`` that aranges its child widgets horizontally and
+    vertically.
+
+    If enough horizontal space is available, it looks like an ``HBoxLayout``,
+    but if enough space is lacking, it automatically wraps its children into
+    multiple rows.
+
+    """
+    heightChanged = pyqtSignal(int)
+
+    def __init__(self, parent=None, margin=0, spacing=-1):
+        super().__init__(parent)
+        if parent is not None:
+            self.setContentsMargins(margin, margin, margin, margin)
+        self.setSpacing(spacing)
+
+        self._item_list = []
+
+    def __del__(self):
+        while self.count():
+            self.takeAt(0)
+
+    def addItem(self, item):  # pylint: disable=invalid-name
+        self._item_list.append(item)
+
+    def addSpacing(self, size):  # pylint: disable=invalid-name
+        self.addItem(QSpacerItem(size, 0, QSizePolicy.Fixed, QSizePolicy.Minimum))
+
+    def count(self):
+        return len(self._item_list)
+
+    def itemAt(self, index):  # pylint: disable=invalid-name
+        if 0 <= index < len(self._item_list):
+            return self._item_list[index]
+        return None
+
+    def takeAt(self, index):  # pylint: disable=invalid-name
+        if 0 <= index < len(self._item_list):
+            return self._item_list.pop(index)
+        return None
+
+    def expandingDirections(self):  # pylint: disable=invalid-name,no-self-use
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self):  # pylint: disable=invalid-name,no-self-use
+        return True
+
+    def heightForWidth(self, width):  # pylint: disable=invalid-name
+        height = self._do_layout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):  # pylint: disable=invalid-name
+        super().setGeometry(rect)
+        self._do_layout(rect, False)
+
+    def sizeHint(self):  # pylint: disable=invalid-name
+        return self.minimumSize()
+
+    def minimumSize(self):  # pylint: disable=invalid-name
+        size = QSize()
+
+        for item in self._item_list:
+            minsize = item.minimumSize()
+            extent = item.geometry().bottomRight()
+            size = size.expandedTo(QSize(minsize.width(), extent.y()))
+
+        margin = self.contentsMargins().left()
+        size += QSize(2 * margin, 2 * margin)
+        return size
+
+    def _do_layout(self, rect, test_only=False):
+        m = self.contentsMargins()
+        effective_rect = rect.adjusted(+m.left(), +m.top(), -m.right(), -m.bottom())
+        x = effective_rect.x()
+        y = effective_rect.y()
+        line_height = 0
+
+        for item in self._item_list:
+            wid = item.widget()
+
+            space_x = self.spacing()
+            space_y = self.spacing()
+            if wid is not None:
+                space_x += wid.style().layoutSpacing(
+                    QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+                space_y += wid.style().layoutSpacing(
+                    QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+
+            next_x = x + item.sizeHint().width() + space_x
+            if next_x - space_x > effective_rect.right() and line_height > 0:
+                x = effective_rect.x()
+                y = y + line_height + space_y
+                next_x = x + item.sizeHint().width() + space_x
+                line_height = 0
+
+            if not test_only:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = next_x
+            line_height = max(line_height, item.sizeHint().height())
+
+        new_height = y + line_height - rect.y()
+        self.heightChanged.emit(new_height)
+        return new_height
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication, QPushButton, QScrollArea, QVBoxLayout, QWidget, QGroupBox
+
+    app = QApplication(sys.argv)
+
+    container = QWidget()
+    container_layout = QVBoxLayout()
+    for i in range(2):
+        g = QGroupBox(f'Group {i}')
+        l = FlowLayout(margin=10)
+        l.heightChanged.connect(container.setMinimumHeight)
+        g.setLayout(l)
+        l.addWidget(QPushButton('Short'))
+        l.addWidget(QPushButton('Longer'))
+        l.addWidget(QPushButton('Different text'))
+        l.addWidget(QPushButton('More text'))
+        l.addWidget(QPushButton('Even longer button text'))
+        container_layout.addWidget(g)
+    container_layout.addStretch()
+    container.setLayout(container_layout)
+
+    w = QScrollArea()
+    w.setWindowTitle('Flow Layout')
+    w.setWidgetResizable(True)
+    w.setWidget(container)
+    w.show()
+
+    sys.exit(app.exec_())
