c6398857944b88e3612e7c92939a96bc7638e78d
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-07-13 14:13:35 +0800

    Add perfect auto complete stock selector.
    Change-Id: Ia54ef46e4882554e3de2c8d07890717c705c9ecd

---
+++ Demo/demo_combobox_completer.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QSortFilterProxyModel
+from PyQt5.QtWidgets import QCompleter, QComboBox
+
+
+# https://stackoverflow.com/a/50639066/12929244
+
+class ExtendedComboBox(QComboBox):
+    def __init__(self, parent=None):
+        super(ExtendedComboBox, self).__init__(parent)
+
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.setEditable(True)
+
+        # add a filter model to filter matching items
+        self.pFilterModel = QSortFilterProxyModel(self)
+        self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.pFilterModel.setSourceModel(self.model())
+
+        # add a completer, which uses the filter model
+        self.completer = QCompleter(self.pFilterModel, self)
+        # always show all (filtered) completions
+        self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+        self.setCompleter(self.completer)
+
+        # connect signals
+        self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
+        self.completer.activated.connect(self.on_completer_activated)
+
+
+    # on selection of an item from the completer, select the corresponding item from combobox 
+    def on_completer_activated(self, text):
+        if text:
+            index = self.findText(text)
+            self.setCurrentIndex(index)
+            self.activated[str].emit(self.itemText(index))
+
+
+    # on model change, update the models of the filter and completer as well 
+    def setModel(self, model):
+        super(ExtendedComboBox, self).setModel(model)
+        self.pFilterModel.setSourceModel(model)
+        self.completer.setModel(self.pFilterModel)
+
+
+    # on model column change, update the model column of the filter and completer as well
+    def setModelColumn(self, column):
+        self.completer.setCompletionColumn(column)
+        self.pFilterModel.setFilterKeyColumn(column)
+        super(ExtendedComboBox, self).setModelColumn(column)    
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtCore import QStringListModel
+
+    app = QApplication(sys.argv)
+
+    string_list = ['hola muchachos', 'adios amigos', 'hello world', 'good bye']
+
+    combo = ExtendedComboBox()
+
+    # either fill the standard model of the combobox
+    combo.addItems(string_list)
+
+    # or use another model
+    #combo.setModel(QStringListModel(string_list))
+
+    combo.resize(300, 40)
+    combo.show()
+
+    sys.exit(app.exec_())
--- StockAnalysisSystem/core/Utiltity/securities_selector.py
+++ StockAnalysisSystem/core/Utiltity/securities_selector.py
@@ -1,12 +1,14 @@
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QComboBox
+
+from PyQt5.QtCore import Qt, QTimer, QSortFilterProxyModel
+from PyQt5.QtWidgets import QCompleter, QComboBox
+from StockAnalysisSystem.core.DataHub.DataUtility import DataUtility
 
 
 class SecuritiesSelector(QComboBox):
-    def __init__(self, data_utility, parent=None):
+    def __init__(self, data_utility: DataUtility, parent=None):
         super(SecuritiesSelector, self).__init__(parent)
 
-        self.__data_utility = data_utility
+        self.__data_utility: DataUtility = data_utility
 
         self.__timer = QTimer()
         self.__timer.setInterval(1000)
@@ -15,7 +17,24 @@ class SecuritiesSelector(QComboBox):
         if self.__data_utility is not None:
             # Timer for update stock list
             self.__timer.start()
+
+        self.setFocusPolicy(Qt.StrongFocus)
         self.setEditable(True)
+
+        # add a filter model to filter matching items
+        self.pFilterModel = QSortFilterProxyModel(self)
+        self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.pFilterModel.setSourceModel(self.model())
+
+        # add a completer, which uses the filter model
+        self.completer = QCompleter(self.pFilterModel, self)
+        # always show all (filtered) completions
+        self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+        self.setCompleter(self.completer)
+
+        # connect signals
+        self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
+        self.completer.activated.connect(self.on_completer_activated)
 
     def on_timer(self):
         # Check stock list ready and update combobox
@@ -26,6 +45,27 @@ class SecuritiesSelector(QComboBox):
                 self.addItem(stock_identity + ' | ' + stock_name, stock_identity)
             self.__timer.stop()
 
+    # on selection of an item from the completer, select the corresponding item from combobox
+    def on_completer_activated(self, text: str):
+        if self.__data_utility is None or \
+                not self.__data_utility.stock_cache_ready():
+            return
+        if text is not None and text != '':
+            self.__data_utility.guess_securities(text)
+            index = self.findData(text)
+            self.setCurrentIndex(index)
+            self.activated[str].emit(self.itemText(index))
+
+    def setModel(self, model):
+        super(QComboBox, self).setModel(model)
+        self.pFilterModel.setSourceModel(model)
+        self.completer.setModel(self.pFilterModel)
+
+    def setModelColumn(self, column):
+        self.completer.setCompletionColumn(column)
+        self.pFilterModel.setFilterKeyColumn(column)
+        super(QComboBox, self).setModelColumn(column)
+
     def select_security(self, security: str, linkage: bool):
         for index in range(self.count()):
             stock_identity = self.itemData(index)
@@ -34,9 +74,51 @@ class SecuritiesSelector(QComboBox):
                 break
 
     def get_input_securities(self) -> str:
-        input_securities = self.currentText()
-        if '|' in input_securities:
-            input_securities = input_securities.split('|')[0].strip()
-        return input_securities
+        select_index = self.currentIndex()
+        if select_index >= 0:
+            return self.itemData(select_index)
+        else:
+            input_securities = self.currentText()
+            if '|' in input_securities:
+                input_securities = input_securities.split('|')[0].strip()
+            return input_securities
+
+
+# class SecuritiesSelector(QComboBox):
+#     def __init__(self, data_utility, parent=None):
+#         super(SecuritiesSelector, self).__init__(parent)
+#
+#         self.__data_utility = data_utility
+#
+#         self.__timer = QTimer()
+#         self.__timer.setInterval(1000)
+#         self.__timer.timeout.connect(self.on_timer)
+#
+#         if self.__data_utility is not None:
+#             # Timer for update stock list
+#             self.__timer.start()
+#         self.setEditable(True)
+#
+#     def on_timer(self):
+#         # Check stock list ready and update combobox
+#         if self.__data_utility is not None and \
+#                 self.__data_utility.stock_cache_ready():
+#             stock_list = self.__data_utility.get_stock_list()
+#             for stock_identity, stock_name in stock_list:
+#                 self.addItem(stock_identity + ' | ' + stock_name, stock_identity)
+#             self.__timer.stop()
+#
+#     def select_security(self, security: str, linkage: bool):
+#         for index in range(self.count()):
+#             stock_identity = self.itemData(index)
+#             if stock_identity == security:
+#                 self.setCurrentIndex(index)
+#                 break
+#
+#     def get_input_securities(self) -> str:
+#         input_securities = self.currentText()
+#         if '|' in input_securities:
+#             input_securities = input_securities.split('|')[0].strip()
+#         return input_securities
 
 
