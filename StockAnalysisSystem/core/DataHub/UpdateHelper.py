295899327d06dc199c208546bba5c70322381bc9
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-09-02 17:05:16 +0800

    Stash
    Change-Id: Ia2820189a8aa23bc306c49de05bb433c9508eed2

--- StockAnalysisSystem/core/DataHub/UpdateHelper.py
+++ StockAnalysisSystem/core/DataHub/UpdateHelper.py
@@ -7,6 +7,8 @@ from ..DataHub.UniversalDataCenter import UniversalDataCenter
 from concurrent.futures import ThreadPoolExecutor, Future, ProcessPoolExecutor, Executor
 
 
+# ---------------------------------------------------- UpdateHelper ----------------------------------------------------
+
 class UpdateHelper:
     UPDATE_ITEM_NONE = 0
     UPDATE_ITEM_AUTO = 1
@@ -188,3 +190,116 @@ class UpdateHelper:
             print('Persistence count: ' + str(counter[UpdateHelper.APPLY_COUNT_INDEX]))
         return True
 
+
+# ----------------------------------------------------- UpdateTask -----------------------------------------------------
+
+class UpdateTask(TaskQueue.Task):
+    def __init__(self, update_helper: UpdateHelper):
+        super(UpdateTask, self).__init__('UpdateTask')
+
+        self.__update_helper = update_helper
+        self.__quit_flag = [False]
+
+        # Parameters
+        self.uri = ''
+        self.identities = []
+        self.clock = Clock(False)
+        self.progress = ProgressRate()
+
+    def in_work_package(self, uri: str) -> bool:
+        return selfuri == uri
+
+    def set_work_package(self, agent: DataAgent, identities: list or str or None):
+        if isinstance(identities, str):
+            identities = [identities]
+        self.identities = identities
+        self.agent = agent
+
+    def run(self):
+        print('Update task start.')
+
+        self.__patch_count = 0
+        self.__apply_count = 0
+        try:
+            # Catch "pymongo.errors.ServerSelectionTimeoutError: No servers found yet" exception and continue.
+            self.__execute_update()
+        except Exception as e:
+            print('Update got Exception: ')
+            print(e)
+            print(traceback.format_exc())
+            print('Continue...')
+        finally:
+            if self.__future is not None:
+                self.__future.cancel()
+        print('Update task finished.')
+
+    def quit(self):
+        self.__quit = True
+
+    def identity(self) -> str:
+        return self.agent.base_uri() if self.agent is not None else ''
+
+    # ------------------------------------- Task -------------------------------------
+
+    def __execute_update(self):
+        # Get identities here to ensure we can get the new list after stock info updated
+        update_list = self.identities if self.identities is not None and len(self.identities) > 0 else \
+                      self.agent.update_list()
+        if update_list is None or len(update_list) == 0:
+            update_list = [None]
+        progress = len(update_list)
+
+        self.clock.reset()
+        self.progress.reset()
+        self.progress.set_progress(self.agent.base_uri(), 0, progress)
+
+        for identity in update_list:
+            while (self.__patch_count - self.__apply_count > 20) and not self.__quit:
+                time.sleep(0.5)
+                continue
+            if self.__quit:
+                break
+
+            print('------------------------------------------------------------------------------------')
+
+            if identity is not None:
+                # Optimise: Update not earlier than listing date.
+                listing_date = self.__data_hub.get_data_utility().get_securities_listing_date(identity, default_since())
+
+                if self.__force:
+                    since, until = listing_date, now()
+                else:
+                    since, until = self.__data_center.calc_update_range(self.agent.base_uri(), identity)
+                    since = max(listing_date, since)
+                time_serial = (since, until)
+            else:
+                time_serial = None
+
+            patch = self.__data_center.build_local_data_patch(
+                self.agent.base_uri(), identity, time_serial, force=self.__force)
+            self.__patch_count += 1
+            print('Patch count: ' + str(self.__patch_count))
+
+            self.__future = self.__pool.submit(self.__execute_persistence,
+                                               self.agent.base_uri(), identity, patch)
+
+        if self.__future is not None:
+            print('Waiting for persistence task finish...')
+            self.__future.result()
+        self.clock.freeze()
+        # self.__ui.task_finish_signal[UpdateTask].emit(self)
+
+    def __execute_persistence(self, uri: str, identity: str, patch: tuple) -> bool:
+        try:
+            if patch is not None:
+                self.__data_center.apply_local_data_patch(patch)
+            if identity is not None:
+                self.progress.set_progress([uri, identity], 1, 1)
+            self.progress.increase_progress(uri)
+        except Exception as e:
+            print('e')
+            return False
+        finally:
+            self.__apply_count += 1
+            print('Persistence count: ' + str(self.__apply_count))
+        return True
