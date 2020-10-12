c449dae2c6423e42a5c57eefce71abb55b2eb059
Yufei QIU <yufei.qiu@schneider-electric.com>
2020-10-12 16:27:33 +0800

    Add test code
    Change-Id: Ied456600557f7d4d3941b12835d30df8117a1dde

--- StockAnalysisSystem/wechatservice/route.py
+++ StockAnalysisSystem/wechatservice/route.py
@@ -9,20 +9,44 @@ wechat: WeChat = None
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def handle_command(msg_dict: dict) -> (bool, str):
+
+def do_test():
+    user_mgr = wechat.get_user_manager()
+    user_lst = user_mgr.get_user_list()
+    if len(user_lst) > 0:
+        wechat.send_user_message(user_lst[0], 'Hello from Sleepy')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def parse_command(text: str) -> (str, str):
+    parts = text.split(':')
+    command = (parts[0] if len(parts) > 0 else '').strip()
+    parameters = (parts[1] if len(parts) > 1 else '').strip()
+    return command, parameters
+
+
+def handle_command(flask_request: request, msg_dict: dict) -> (bool, str):
+    content: str = msg_dict.get('Content', '')
+    command, parameters = parse_command(content)
+
+    if command == 'test':
+        do_test()
+        return True, ''
+
     return False, ''
 
 
-def handle_analysis(msg_dict: dict) -> (bool, str):
-    content = msg_dict.get('Content')
+def handle_analysis(flask_request: request, msg_dict: dict) -> (bool, str):
+    content = msg_dict.get('Content', '')
     return True, ('<a href="http://211.149.229.160/analysis?security=%s">查看分析结果</a>' % content)
 
 
 def handle_text_message(flask_request: request, msg_dict: dict) -> str:
-    ret, resp = handle_command(msg_dict)
+    ret, resp = handle_command(flask_request, msg_dict)
     if ret:
         return resp
-    ret, resp = handle_analysis(msg_dict)
+    ret, resp = handle_analysis(flask_request, msg_dict)
     if ret:
         return resp
     return ''
