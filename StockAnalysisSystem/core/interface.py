89e46de484d9fbc734e6c13f98e56ab1e02486a9
Sleepy <sleepysoft@gmail.com>
2020-10-29 21:33:11 +0800

    Debug webapi

--- StockAnalysisSystem/core/interface.py
+++ StockAnalysisSystem/core/interface.py
@@ -27,16 +27,16 @@ def sas_update(uri: str, identity: str or [str] = None, time_serial: tuple = Non
     return data_center.update_local_data(uri, identity, time_serial, **extra)
 
 
-def sas_analysis(securities: str or [str], analyzers: [str],
-                 time_serial: (datetime.datetime, datetime.datetime),
-                 progress_rate: ProgressRate = None,
-                 enable_calculation: bool = True,
-                 enable_from_cache: bool = True, enable_update_cache: bool = True,
-                 debug_load_json: bool = False, debug_dump_json: bool = False,
-                 dump_path: str = '') -> pd.DataFrame:
-    from .AnalyzerEntry import StrategyEntry
-    strategy_entry: StrategyEntry = sas().get_strategy_entry()
-    strategy_entry.analysis_advance()
+# def sas_analysis(securities: str or [str], analyzers: [str],
+#                  time_serial: (datetime.datetime, datetime.datetime),
+#                  progress_rate: ProgressRate = None,
+#                  enable_calculation: bool = True,
+#                  enable_from_cache: bool = True, enable_update_cache: bool = True,
+#                  debug_load_json: bool = False, debug_dump_json: bool = False,
+#                  dump_path: str = '') -> pd.DataFrame:
+#     from .AnalyzerEntry import StrategyEntry
+#     strategy_entry: StrategyEntry = sas().get_strategy_entry()
+#     strategy_entry.analysis_advance()
 
 
 def sas_get_all_uri() -> [str]:
