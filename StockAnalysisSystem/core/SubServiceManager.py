b689ae131df8d3d5c2f1a7ed49782ea20fdd246e
Sleepy <sleepysoft@gmail.com>
2021-02-06 21:26:15 +0800

    Update sub service manager.

--- StockAnalysisSystem/core/SubServiceManager.py
+++ StockAnalysisSystem/core/SubServiceManager.py
@@ -1,7 +1,6 @@
 import time
+from functools import partial
 from threading import Thread
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QWidget
 
 import StockAnalysisSystem.core.api as sasApi
 from StockAnalysisSystem.core.Utility.event_queue import *
@@ -10,7 +9,54 @@ from StockAnalysisSystem.core.Utility.plugin_manager import *
 
 
 class SubServiceManager:
-    class SubServiceEventWrapper(Event):
+
+    class SubServiceWrapper:
+        """
+        This wrapper and invoke plugin function directly.
+        """
+        def __init__(self, plugin: PluginManager, extension: any):
+            self.__data = dict()
+            self.__plugin = plugin
+            self.__extension = extension
+
+        def __getattr__(self, attr):
+            return partial(self.invoke, attr)
+
+        def set_data(self, k: str, v: any):
+            self.__data[k] = v
+
+        def get_data(self, k: str) -> any:
+            return self.__data.get(k, None)
+
+        def invoke(self, func: str, *args, **kwargs) -> any:
+            try:
+                result = self.__plugin.execute_module_function(self.__extension, func, kwargs)
+                return result
+            except Exception as e:
+                print('Invoke error: ' + str(e))
+                print(traceback.format_exc())
+                return None
+            finally:
+                pass
+
+        def extension(self) -> any:
+            return self.__extension
+
+    class SubServiceThreadWrapper(Thread):
+        def __init__(self, plugin: PluginManager, extension: any):
+            super(SubServiceManager.SubServiceThreadWrapper, self).__init__()
+            self.__plugin = plugin
+            self.__service = extension
+            self.__context = {'quit_flag': False}
+
+        def run(self):
+            ret = self.__plugin.execute_module_function(self.__service, 'thread', {'context': self.__context})
+            print('Sub Service thread exit, return = ' + str(ret))
+
+        def quit(self):
+            self.__context['quit_flag'] = True
+
+    class SubServiceEventWrapper(EventHandler):
         def __init__(self, plugin: PluginManager, identity: str, extension):
             self.__plugin = plugin
             self.__identity = identity
@@ -23,30 +69,17 @@ class SubServiceManager:
         def handle_event(self, event: Event):
             self.__plugin.execute_module_function(self.__extension, 'event_handler', {'event': event})
 
-    class SubServiceThreadWrapper(Thread):
-        def __init__(self, plugin: PluginManager, extension: any):
-            super(ExtensionManager.SubServiceThreadWrapper, self).__init__()
-            self.__plugin = plugin
-            self.__service = extension
-            self.__context = {'quit_flag': False}
-
-        def run(self):
-            ret = self.__plugin.execute_module_function(self.__service, 'thread', {'context': self.__context})
-            print('Sub Service thread exit, return = ' + str(ret))
-
-        def quit(self):
-            self.__context['quit_flag'] = True
-
     def __init__(self, sas_api: sasApi, plugin: PluginManager):
         self.__sas_api = sas_api
         self.__plugin = plugin
 
         self.__event_queue = EventQueue()
 
+        self.__service_table = {}
+
         # Threads
         self.__service_threads = {}
 
-        self.__service_cap = {}
         self.__period_service = []
         self.__thread_service = []
         self.__event_handler_service = []
@@ -55,45 +88,52 @@ class SubServiceManager:
         self.__plugin.refresh()
         modules = self.__plugin.all_modules()
         for module in modules:
-            result = self.__plugin.execute_module_function(module, 'plugin_capacities', {})
-            if len(result) == 0:
+            service_wrapper = SubServiceManager.SubServiceWrapper(self.__plugin, module)
+
+            prob = service_wrapper.plugin_prob()
+            if prob is None or 'plugin_id' not in prob or 'plugin_name' not in prob:
                 continue
-            capacities = result[0]
-            if capacities is not None and isinstance(capacities, (list, tuple)):
-                if 'thread' in capacities:
-                    self.__thread_service.append(module)
-                if 'polling' in capacities:
-                    self.__period_service.append(module)
-                if 'event_handler' in capacities:
-                    self.__event_handler_service.append(module)
-                self.__service_cap[module] = capacities
+
+            capacities = service_wrapper.plugin_capacities()
+            if capacities is None or not isinstance(capacities, (list, tuple)) or len(capacities) == 0:
+                continue
+
+            if 'thread' in capacities:
+                self.__thread_service.append(service_wrapper)
+            if 'polling' in capacities:
+                self.__period_service.append(service_wrapper)
+            if 'event_handler' in capacities:
+                self.__event_handler_service.append(service_wrapper)
+            self.__service_table[prob['plugin_id']] = service_wrapper
+
         return self.init_services() and self.activate_services()
 
     def init_services(self) -> bool:
         fail_service = []
-        for extension in self.__service_cap.keys():
-            result = self.__plugin.execute_module_function(extension, 'init', {'sas_api': self.__sas_api})
+        for service_wrapper in self.__service_table.keys():
+            result = service_wrapper.init(sas_api=self.__sas_api)
             if len(result) == 0 or not result[0]:
-                fail_service.append(extension)
-                capacity = self.__service_cap.get(extension, {})
-                if isinstance(capacity, dict):
-                    print('Fail to init extension: ' + capacity.get('plugin_id', 'NO ID'))
+                fail_service.append(service_wrapper)
+                prob = service_wrapper.plugin_prob()
+                if isinstance(prob, dict):
+                    print('Fail to init extension: ' + prob.get('plugin_id', 'NO ID'))
                 else:
                     print('Fail to init extension: prob data error')
         for fail_ext in fail_service:
-            del self.__service_cap[fail_ext]
             if fail_ext in self.__period_service:
                 self.__period_service.remove(fail_ext)
             if fail_ext in self.__thread_service:
                 self.__thread_service.remove(fail_ext)
             if fail_ext in self.__event_handler_service:
                 self.__event_handler_service.remove(fail_ext)
+            # TODO: Delete from self.__service_table by value
         return True
 
     def activate_services(self) -> bool:
-        self.__timer.start()
-        self.__run_thread_services()
-        return True
+        ret = True
+        ret = ret and self.__register_event_handler_service()
+        ret = ret and self.__run_thread_services()
+        return ret
 
     def drive_service(self):
         pass
@@ -101,23 +141,25 @@ class SubServiceManager:
     # ------------------------------------------------------------------------------------------------
 
     def __register_event_handler_service(self) -> bool:
-        for extension in self.__event_handler_service:
-            self.__event_queue.add_event_handler(SubServiceManager.SubServiceEventWrapper(extension))
+        for service_wrapper in self.__event_handler_service:
+            self.__event_queue.add_event_handler(
+                SubServiceManager.SubServiceEventWrapper(service_wrapper.extension))
 
     def __run_thread_services(self) -> bool:
-        for extension in self.__thread_service:
-            extension_thread = ExtensionManager.SubServiceThreadWrapper(self.__plugin, extension)
-            self.__service_threads[extension] = extension_thread
+        for service_wrapper in self.__thread_service:
+            extension_thread = SubServiceManager.SubServiceThreadWrapper(
+                self.__plugin, service_wrapper.extension)
+            service_wrapper.set_data('extension_thread', extension_thread)
             extension_thread.start()
         return True
 
-    def __poll_period_services(self) -> bool:
+    def __poll_services(self) -> bool:
         tick_ns = time.time() * 1000000
         if self.__prev_tick == 0:
             self.__prev_tick = tick_ns
         elapsed_ns = tick_ns - self.__prev_tick
-        for extension in self.__period_service:
-            self.__plugin.execute_module_function(extension, 'period', {'interval_ns': elapsed_ns})
+        for service_wrapper in self.__period_service:
+            service_wrapper.polling(elapsed_ns)
         self.__prev_tick = tick_ns
         return True
 
