c457548c281152452e2d3bd3160f12b3c6a0fcda
zhangshen <zhangshen@chaintor.com>
2021-03-02 16:54:40 +0800

    '半路撤离'

--- data_01.py
+++ data_01.py
@@ -4,7 +4,7 @@ from StockAnalysisSystem.core.DataHub.DataAgent import DataAgent
 from StockAnalysisSystem.core.DataHub import DataAgentBuilder
 from StockAnalysisSystem.core.UniversalDataDepot import DepotMongoDB
 from pymongo import MongoClient
-
+from StockAnalysisSystem.plugin.Collector import finance_data_tushare_pro as fp
 
 DATA_DURATION_QUARTER = 500
 mongodb_client = MongoClient()
@@ -30,16 +30,17 @@ print(share_float)
 
 
 data_repurchase = DataAgent(
-    uri='Finance.IncomeStatement',
+    uri='Finance.IncomeStatement',  # 用以标识这个数据，标识符
+    # 使用的数据库类型
     depot=DepotMongoDB(primary_keys=['stock_identity', 'period'],
                        client=mongodb_client,
                        database='StockAnalysisSystem',
                        data_table=DataAgentBuilder.uri_to_table('Finance. IncomeStatement')),
     identity_field='stock_identity',
     datetime_field='period',
-    data_duration=DATA_DURATION_QUARTER,
-    update_list=DataAgentUtility.a_stock_list,
-    ),
+    data_duration=DATA_DURATION_QUARTER,  # 数据周期
+    update_list=DataAgentUtility.a_stock_list,  # 更新的子列表
+    )
 
 data_share_float = DataAgent(
     uri='Market. SecuritiesInfo',
@@ -51,14 +52,31 @@ data_share_float = DataAgent(
     datetime_field=None,
     data_duration=DATA_DURATION_QUARTER,
     update_priority=DataAgent.PRIORITY_HIGHEST
-    ),
+    )
+
+fp.plugin_prob()
+fp.plugin_adapt(data_repurchase.base_uri())
+fp.plugin_capacities()
+
+fp.query()
+
+
+# StockAnalysisSystem/core/DataHub/UniversalDataCenter.py
+from StockAnalysisSystem.core.DataHub.UniversalDataCenter import UniversalDataCenter
+# StockAnalysisSystem/core/Database/DatabaseEntry.py
+from StockAnalysisSystem.core.Database.DatabaseEntry import DatabaseEntry
+# StockAnalysisSystem/core/Utility/plugin_manager.py
+from StockAnalysisSystem.core.Utility.plugin_manager import PluginManager
+
+udc = UniversalDataCenter(DatabaseEntry(), PluginManager())
 
 
 
+# StockAnalysisSystem/core/Database/NoSqlRw.py
+from StockAnalysisSystem.core.Database import NoSqlRw
 
-
-
-
+# it_table = NoSqlRw.ItkvTable(client=mongodb_client,
+#                              database=)
 
 
 
