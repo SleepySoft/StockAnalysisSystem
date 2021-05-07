44d019ef4dad15bd501a6cc3bf8ced44ae723ce0
Yufei QIU <yufei.qiu@schneider-electric.com>
2021-05-07 12:01:30 +0800

    Add PyWeChatSpy and debug OK
    Change-Id: I3e4e7a70398d3b2c3c532dabc3c1162fd432113b

---
+++ StockAnalysisSystem/plugin/SubService/PyWeChatSpy/spy.py
@@ -0,0 +1,333 @@
+import os
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+import logging
+from .proto import spy_pb2
+from .command import *
+import subprocess
+from queue import Queue
+from uuid import uuid4
+import sys
+
+
+if not sys.version >= "3.8":
+    logging.error("微信版本过低，请使用Python3.8.x或更高版本")
+    exit()
+
+
+class WeChatSpy:
+    def __init__(self, response_queue=None, key: str = None, logger: logging.Logger = None):
+        # 付费key
+        self.__key = key
+        # 日志模块
+        if isinstance(logger, logging.Logger):
+            # 使用自定义logger
+            self.logger = logger
+        else:
+            # 使用默认logger
+            self.logger = logging.getLogger(__file__)
+            formatter = logging.Formatter('%(asctime)s [%(threadName)s] %(levelname)s: %(message)s')
+            sh = logging.StreamHandler()
+            sh.setFormatter(formatter)
+            sh.setLevel(logging.DEBUG)
+            self.logger.addHandler(sh)
+            self.logger.setLevel(logging.DEBUG)
+        # response存放队列
+        if isinstance(response_queue, Queue):
+            self.__response_queue = response_queue
+        else:
+            raise Exception("response_queue must be Queue")
+        self.pids = []
+        self.port2client = dict()
+        host = "127.0.0.1"
+        port = 9527
+        self.__socket_server = socket(AF_INET, SOCK_STREAM)
+        self.__socket_server.bind((host, port))
+        self.__socket_server.listen(1)
+        t_start_server = Thread(target=self.__start_server)
+        t_start_server.daemon = True
+        t_start_server.name = "spy"
+        t_start_server.start()
+        current_path = os.path.split(os.path.abspath(__file__))[0]
+        helper_path = os.path.join(current_path, "SpyK.exe")
+        if not os.path.exists(helper_path):
+            self.logger.error("请检查文件 SpyK.exe 是否被误删")
+            exit(-1)
+        subprocess.Popen(f"{helper_path} 3.0")
+
+    def __start_server(self):
+        while True:
+            socket_client, client_address = self.__socket_server.accept()
+            self.port2client[client_address[1]] = socket_client
+            self.logger.debug(f"A WeChat process from {client_address} successfully connected")
+            if self.__key:
+                self.set_commercial(self.__key, port=client_address[1])
+            t_socket_client_receive = Thread(target=self.receive, args=(socket_client, client_address))
+            t_socket_client_receive.name = f"wechat {client_address}"
+            t_socket_client_receive.daemon = True
+            t_socket_client_receive.start()
+
+    def receive(self, socket_client: socket, client_address: tuple):
+        recv_bytes = b""
+        data_size = 0
+        while True:
+            try:
+                _bytes = socket_client.recv(4096)
+            except Exception as e:
+                self.port2client.pop(client_address[1])
+                response = spy_pb2.Response()
+                response.type = WECHAT_DISCONNECT
+                response.port = client_address[1]
+                self.__response_queue.put(response)
+                return self.logger.warning(f"The WeChat process has disconnected: {e}")
+            recv_bytes += _bytes
+            while True:
+                if not data_size:
+                    if len(recv_bytes) > 3:
+                        data_size = int.from_bytes(recv_bytes[:4], "little")
+                    else:
+                        break
+                elif data_size <= len(recv_bytes) - 4:
+                    response = spy_pb2.Response()
+                    response.ParseFromString(recv_bytes[4: data_size + 4])
+                    response.port = client_address[1]
+                    self.__response_queue.put(response)
+                    recv_bytes = recv_bytes[data_size + 4:]
+                    data_size = 0
+                else:
+                    break
+
+    def __send(self, request: spy_pb2.Request, port: int = 0, _id: str = None):
+        if not port and self.port2client:
+            socket_client: socket = list(self.port2client.values())[0]
+        elif not (socket_client := self.port2client.get(port)):
+            self.logger.error(f"Failure to find socket client by port:{port}")
+            return False
+        request.id = _id or uuid4().__str__()
+        data = request.SerializeToString()
+        data_length_bytes = int.to_bytes(len(data), length=4, byteorder="little")
+        try:
+            socket_client.send(data_length_bytes + data)
+            return True
+        except Exception as e:
+            self.logger.warning(f"The WeChat process {port} has disconnected: {e}")
+            return False
+
+    def run(self, wechat: str):
+        sp = subprocess.Popen(wechat)
+        self.pids.append(sp.pid)
+        return sp.pid
+
+    def set_commercial(self, key: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = PROFESSIONAL_KEY
+        request.bytes = bytes(key, encoding="utf8")
+        return self.__send(request, port, _id)
+
+    def get_account_details(self, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = GET_ACCOUNT_DETAILS
+        return self.__send(request, port, _id)
+
+    def get_contacts(self, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = GET_CONTACTS_LIST
+        return self.__send(request, port, _id)
+
+    def get_contact_details(self, wxid: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = GET_CONTACT_DETAILS
+        request.bytes = bytes(wxid, encoding="utf8")
+        return self.__send(request, port, _id)
+
+    def send_text(self, wxid: str, text: str, at_wxid: str = "", port: int = 0, _id: str = None):
+        if not wxid.endswith("chatroom"):
+            at_wxid = ""
+        request = spy_pb2.Request()
+        request.type = SEND_TEXT
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.wxidAt = at_wxid
+        text_message.text = text
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def send_file(self, wxid: str, file_path: str, port: int = 0, _id: str = None):
+        if not os.path.exists(file_path):
+            self.logger.error(f"File Not Found {file_path}")
+            return False
+        if len(file_path.split("\\")) > 8:
+            self.logger.error(f"File path is too long: {file_path}")
+            return False
+        request = spy_pb2.Request()
+        request.type = SEND_FILE
+        file_message = spy_pb2.FileMessage()
+        file_message.wxid = wxid
+        file_message.filePath = file_path
+        request.bytes = file_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def user_logout(self, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = USER_LOGOUT
+        return self.__send(request, port, _id)
+
+    def accept_new_contact(self, encryptusername: str, ticket: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = ACCEPT_NEW_CONTACT
+        application = spy_pb2.ContactApplication()
+        application.encryptusername = encryptusername
+        application.ticket = ticket
+        request.bytes = application.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def send_announcement(self, wxid: str, content: str, port: int = 0, _id: str = None):
+        if not wxid.endswith("chatroom"):
+            return self.logger.warning("Can only send announcements to chatrooms")
+        request = spy_pb2.Request()
+        request.type = SEND_ANNOUNCEMENT
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.text = content
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def create_chatroom(self, wxid: str, port: int = 0, _id: str = None):
+        if len(wxid.split(",")) < 2:
+            return self.logger.warning("This function requires at least two wxids separated by ','")
+        request = spy_pb2.Request()
+        request.type = CREATE_CHATROOM
+        request.bytes = bytes(wxid, encoding="utf8")
+        return self.__send(request, port, _id)
+
+    def share_chatroom(self, chatroom_wxid: str, wxid: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = SHARE_CHATROOM
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.text = chatroom_wxid
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def remove_chatroom_member(self, chatroom_wxid: str, wxid: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = REMOVE_CHATROOM_MEMBER
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.text = chatroom_wxid
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def remove_contact(self, wxid: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = REMOVE_CONTACT
+        request.bytes = bytes(wxid, encoding="utf8")
+        return self.__send(request, port, _id)
+
+    def send_mini_program(self, wxid: str, title: str, image_path: str, route: str, app_id: str,
+                          username: str, weappiconurl: str, appname: str, port: int = 0, _id: str = None):
+        if not os.path.exists(image_path):
+            self.logger.error(f"Image Not Found {image_path}")
+            return False
+        request = spy_pb2.Request()
+        request.type = SEND_MINI_PROGRAM
+        xml = spy_pb2.XmlMessage()
+        xml.wxid = wxid
+        xml.title = title
+        xml.appId = app_id
+        xml.imagePath = image_path
+        xml.route = route
+        xml.username = username
+        xml.weappiconurl = weappiconurl
+        xml.appname = appname
+        request.bytes = xml.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def send_link_card(self, wxid: str, title: str, desc: str, app_id: str,
+                       url: str, image_path: str, port: int = 0, _id: str = None):
+        if not os.path.exists(image_path):
+            self.logger.error(f"Image Not Found {image_path}")
+            return False
+        request = spy_pb2.Request()
+        request.type = SEND_LINK_CARD
+        xml = spy_pb2.XmlMessage()
+        xml.wxid = wxid
+        xml.title = title
+        xml.desc = desc
+        xml.url = url
+        xml.appId = app_id
+        xml.imagePath = image_path
+        request.bytes = xml.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def add_contact(self, wxid: str, chatroom_wxid: str = "", greeting: str = "",
+                    add_type: int = 0, port: int = 0, _id: str = None):
+        # TODO:
+        request = spy_pb2.Request()
+        request.cmd = add_type
+        request.param1 = wxid
+        if add_type == 1 and not chatroom_wxid:
+            return
+        request.param2 = chatroom_wxid
+        request.param3 = greeting
+        return self.__send(request, port, _id)
+
+    def get_contact_status(self, wxid: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = GET_CONTACT_STATUS
+        request.bytes = bytes(wxid, encoding="utf8")
+        return self.__send(request, port, _id)
+
+    def set_chatroom_name(self, wxid: str, name: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = SET_CHATROOM_NAME
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.text = name
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def get_login_qrcode(self, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = GET_LOGIN_QRCODE
+        return self.__send(request, port, _id)
+
+    def set_remark(self, wxid: str, remark: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = SET_REMARK
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.text = remark
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def get_group_enter_url(self, wxid: str, url: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = GET_GROUP_ENTER_URL
+        text_message = spy_pb2.TextMessage()
+        text_message.wxid = wxid
+        text_message.text = url
+        request.bytes = text_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def decrypt_image(self, source_file: str, target_file: str, port: int = 0, _id: str = None):
+        if not os.path.exists(source_file):
+            self.logger.error(f"File Not Found {source_file}")
+            return False
+        request = spy_pb2.Request()
+        request.type = DECRYPT_IMAGE
+        file_message = spy_pb2.FileMessage()
+        file_message.wxid = source_file
+        file_message.filePath = target_file
+        request.bytes = file_message.SerializeToString()
+        return self.__send(request, port, _id)
+
+    def send_card(self, wxid: str, card_wxid: str, card_nickname: str, port: int = 0, _id: str = None):
+        request = spy_pb2.Request()
+        request.type = SEND_CARD
+        xml = spy_pb2.XmlMessage()
+        xml.wxid = wxid
+        xml.title = card_nickname
+        xml.username = card_wxid
+        request.bytes = xml.SerializeToString()
+        return self.__send(request, port, _id)
--- StockAnalysisSystem/plugin/SubService/wechat_robot_service.py
+++ StockAnalysisSystem/plugin/SubService/wechat_robot_service.py
@@ -1,12 +1,236 @@
-from wxpy import *
 import StockAnalysisSystem.core.api as sasApi
 from StockAnalysisSystem.core.Utility.event_queue import Event
 from StockAnalysisSystem.core.SubServiceManager import SubServiceContext
 
+import os
+import sys
+import time
+import shutil
+import logging
+import traceback
+from lxml import etree
+from queue import Queue
+from logging.handlers import TimedRotatingFileHandler
+
+try:
+    from PyWeChatSpy import WeChatSpy
+    from PyWeChatSpy.command import *
+    from PyWeChatSpy.proto import spy_pb2
+    from PyWeChatSpy.games.truth_or_dare import TruthOrDare
+except Exception as e:
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    os.sys.path.append(root_path)
+
+    from PyWeChatSpy import WeChatSpy
+    from PyWeChatSpy.command import *
+    from PyWeChatSpy.proto import spy_pb2
+    from PyWeChatSpy.games.truth_or_dare import TruthOrDare
+finally:
+    pass
+
+
 SERVICE_ID = '12d59df4-c218-45af-9a3e-a0b99526c291'
+
+wechatPid = None
+wechatSpy: WeChatSpy = None
+wechatQueue: Queue = Queue()
+subServiceContext: SubServiceContext = None
 
 
 # ----------------------------------------------------------------------------------------------------------------------
+
+def user_message_handler(sender: str, receiver: str, message: str):
+    print(message)
+
+
+def group_message_handler(sender: str, receiver: str, message: str):
+    from_group_member = message.split(':\n', 1)[0]      # 群内发言人
+    message_content = message.split(':\n', 1)[-1]       # 群聊消息内容
+
+    # Only handle @myself message
+    if message_content.startswith('@StockAnalysisSystem'):
+        print(message_content)
+        at_str = '@' + from_group_member
+        wechatSpy.send_text(sender, 'Hello', at_wxid=at_str)
+
+
+def on_wechat_text_message(sender: str, receiver: str, message: str):
+    print('%s -> %s: %s' % (sender, receiver, message))
+    if sender.endswith("@chatroom"):
+        group_message_handler(sender, receiver, message)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Porting from: https://github.com/veikai/PyWeChatSpy
+#               example.py
+# Python 3.8 or higher is required
+#
+# Why we choose this lib but not wxpy or itchat?
+#     Because wxpy and itchat depends on web wechat, which always being blocked by Tx
+# ----------------------------------------------------------------------------------------------------------------------
+
+if not sys.version >= "3.8":
+    print("如果想使用微信机器人，请升级Python到3.8.x或更高版本")
+    assert False
+
+
+WECHAT_PROFILE = rf"C:\Users\{os.environ['USERNAME']}\Documents\WeChat Files"
+PATCH_PATH = rf"C:\Users\{os.environ['USERNAME']}\AppData\Roaming\Tencent\WeChat\patch"
+
+
+if not os.path.exists(WECHAT_PROFILE):
+    print("请先设置计算机用户名，并完善WECHAT_PROFILE和PATCH_PATH")
+    assert False
+
+
+def handle_response(data):
+    if data.type == PROFESSIONAL_KEY:
+        if not data.code:
+            print(data.message)
+    elif data.type == WECHAT_CONNECTED:  # 微信接入
+        print(f"微信客户端已接入 port:{data.port}")
+        time.sleep(1)
+        # wechatSpy.get_login_qrcode()  # 获取登录二维码
+    elif data.type == HEART_BEAT:  # 心跳
+        pass
+    elif data.type == WECHAT_LOGIN:  # 微信登录
+        print("微信登录")
+        wechatSpy.get_account_details()  # 获取登录账号详情
+    elif data.type == WECHAT_LOGOUT:  # 微信登出
+        print("微信登出")
+    elif data.type == CHAT_MESSAGE:  # 微信消息
+        chat_message = spy_pb2.ChatMessage()
+        chat_message.ParseFromString(data.bytes)
+        for message in chat_message.message:
+            _type = message.type  # 消息类型 1.文本|3.图片...自行探索
+            _from = message.wxidFrom.str  # 消息发送方
+            _to = message.wxidTo.str  # 消息接收方
+            content = message.content.str  # 消息内容
+            # _from_group_member = ""
+            # if _from.endswith("@chatroom"):  # 群聊消息
+            #     _from_group_member = message.content.str.split(':\n', 1)[0]  # 群内发言人
+            #     content = message.content.str.split(':\n', 1)[-1]  # 群聊消息内容
+            # image_overview_size = message.imageOverview.imageSize  # 图片缩略图大小
+            # image_overview_bytes = message.imageOverview.imageBytes  # 图片缩略图数据
+            # with open("img.jpg", "wb") as wf:
+            #     wf.write(image_overview_bytes)
+            overview = message.overview  # 消息缩略
+            timestamp = message.timestamp  # 消息时间戳
+            if _type == 1:  # 文本消息
+                on_wechat_text_message(_from, _to, content)
+                # print(_from, _to, _from_group_member, content)
+                # if _to == "filehelper":
+                #     wechatSpy.send_text("filehelper", "Hello PyWeChatSpy3.0\n" + content)
+                #     time.sleep(2)
+                #     wechatSpy.send_file("filehelper", r"D:\18020891\Pictures\b.jpg")
+                # else:
+                #     wechatSpy.send_text(_from, 'Echo: ' + content)
+            elif _type == 3:  # 图片消息
+                file_path = message.file
+                file_path = os.path.join(WECHAT_PROFILE, file_path)
+                time.sleep(10)
+                # wechatSpy.decrypt_image(file_path, "a.jpg")
+            elif _type == 43:  # 视频消息
+                pass
+            elif _type == 49:  # XML报文消息
+                print(_from, _to, message.file)
+                xml = etree.XML(content)
+                xml_type = xml.xpath("/msg/appmsg/type/text()")[0]
+                if xml_type == "5":
+                    xml_title = xml.xpath("/msg/appmsg/title/text()")[0]
+                    print(xml_title)
+                    if xml_title == "邀请你加入群聊":
+                        url = xml.xpath("/msg/appmsg/url/text()")[0]
+                        print(url)
+                        time.sleep(1)
+                        wechatSpy.get_group_enter_url(_from, url)
+            elif _type == 37:  # 好友申请
+                print("新的好友申请")
+                obj = etree.XML(message.content.str)
+                encryptusername, ticket = obj.xpath("/msg/@encryptusername")[0], obj.xpath("/msg/@ticket")[0]
+                wechatSpy.accept_new_contact(encryptusername, ticket)  # 接收好友请求
+    elif data.type == ACCOUNT_DETAILS:  # 登录账号详情
+        if data.code:
+            account_details = spy_pb2.AccountDetails()
+            account_details.ParseFromString(data.bytes)
+            print(account_details)
+            wechatSpy.get_contacts()  # 获取联系人列表
+        else:
+            print(data.message)
+    elif data.type == CONTACTS_LIST:  # 联系人列表
+        if data.code:
+            contacts_list = spy_pb2.Contacts()
+            contacts_list.ParseFromString(data.bytes)
+            for contact in contacts_list.contactDetails:  # 遍历联系人列表
+                wxid = contact.wxid.str  # 联系人wxid
+                nickname = contact.nickname.str  # 联系人昵称
+                remark = contact.remark.str  # 联系人备注
+                print(wxid, nickname, remark)
+            # wechatSpy.get_contact_details("20646587964@chatroom")  # 获取群聊详情
+        else:
+            print(data.message)
+    elif data.type == CONTACT_DETAILS:
+        if data.code:
+            contact_details_list = spy_pb2.Contacts()
+            contact_details_list.ParseFromString(data.bytes)
+            for contact_details in contact_details_list.contactDetails:  # 遍历联系人详情
+                wxid = contact_details.wxid.str  # 联系人wxid
+                nickname = contact_details.nickname.str  # 联系人昵称
+                remark = contact_details.remark.str  # 联系人备注
+                if wxid.endswith("chatroom"):  # 判断是否为群聊
+                    group_member_list = contact_details.groupMemberList  # 群成员列表
+                    member_count = group_member_list.memberCount  # 群成员数量
+                    for group_member in group_member_list.groupMember:  # 遍历群成员
+                        member_wxid = group_member.wxid  # 群成员wxid
+                        member_nickname = group_member.nickname  # 群成员昵称
+                        # print(member_wxid, member_nickname)
+                    pass
+        else:
+            print(data.message)
+    elif data.type == GET_CONTACTS_LIST and not data.code:
+        print(data.message)
+    elif data.type == CREATE_GROUP_CALLBACK:  # 创建群聊回调
+        callback = spy_pb2.CreateGroupCallback()
+        callback.ParseFromString(data.bytes)
+        print(callback)
+    elif data.type == GROUP_MEMBER_DETAILS:  # 群成员详情
+        group_member_details = spy_pb2.GroupMemberDetails()
+        group_member_details.ParseFromString(data.bytes)
+        # print(group_member_details)
+    elif data.type == GROUP_MEMBER_EVENT:  # 群成员进出事件
+        group_member_event = spy_pb2.GroupMemberEvent()
+        group_member_event.ParseFromString(data.bytes)
+        # print(group_member_event)
+    elif data.type == LOGIN_QRCODE:  # 登录二维码
+        qrcode = spy_pb2.LoginQRCode()
+        qrcode.ParseFromString(data.bytes)
+        with open("qrcode.png", "wb") as _wf:
+            _wf.write(qrcode.qrcodeBytes)
+    elif data.type == GROUP_ENTER_URL:  # 进群链接
+        group_enter_url = spy_pb2.GroupEnterUrl()
+        group_enter_url.ParseFromString(data.bytes)
+        print(group_enter_url)
+        # 进群直接post请求链接
+        # try:
+        #     requests.post(group_enter_url.url)
+        # except requests.exceptions.InvalidSchema:
+        #     pass
+        # except Exception as e:
+        #     logger.error(f"进群失败：{e}")
+    elif data.type == SEND_TEXT_CALLBACK:
+        print("发送文本回调")
+        print(data)
+        print(data.code)
+    elif data.type == SEND_XML_CALLBACK:
+        print("发送xml回调")
+        print(data)
+        print(data.code)
+    elif data.type == SEND_IMAGE_CALLBACK:
+        print("发送图片回调")
+        print(data)
+        print(data.code)
+    else:
+        print(data)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -35,60 +259,57 @@ def plugin_capacities() -> list:
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-wechatRobot: Bot = None
-subServiceContext: SubServiceContext = None
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-def user_message_handler(msg):
-    print(msg)
-
-
-def group_message_handler(msg):
-    print(msg)
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-
 def init(sub_service_context: SubServiceContext) -> bool:
     try:
+        if os.path.isdir(PATCH_PATH):
+            shutil.rmtree(PATCH_PATH)
+        if not os.path.exists(PATCH_PATH):
+            with open(PATCH_PATH, "a") as wf:
+                wf.write("")
+
         global subServiceContext
         subServiceContext = sub_service_context
+
+        global wechatSpy
+        wechatSpy = WeChatSpy(response_queue=wechatQueue, key=None, logger=None)
     except Exception as e:
-        import traceback
         print('Plugin-in init error: ' + str(e))
         print(traceback.format_exc())
+        return False
     finally:
         pass
     return True
 
 
 def startup() -> bool:
-    global wechatRobot
-    wechatRobot = Bot()
-
-    @wechatRobot.register(User, TEXT)
-    def user_message(msg):
-        user_message_handler(msg)
-
-    @wechatRobot.register(Group, TEXT)
-    def group_message(msg):
-        group_message_handler(msg)
-
     return True
 
 
 def thread(context: dict):
-    wechatRobot.join()
+    from queue import Empty
+
+    global wechatPid
+    wechatPid = wechatSpy.run(r"C:\Program Files (x86)\Tencent\WeChat\WeChat.exe")
+
+    while not context['quit_flag']:
+        try:
+            data = wechatQueue.get(timeout=0.5)
+            handle_response(data)
+        except Empty:
+            if context['quit_flag']:
+                break
+            time.sleep(0.1)
+        except Exception as e:
+            print('Wechat robot thread exception: ' + str(e))
+            print(traceback.format_exc())
+        finally:
+            pass
 
 
 def polling(interval_ns: int):
-    print('Period...' + str(interval_ns))
     pass
 
 
 def event_handler(event: Event, **kwargs):
-    print('Event')
     pass
 
